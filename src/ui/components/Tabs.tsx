9725147376cef8b3eded324d275ef5ccd4f12160
Aurélien Grimaud <a.grimaud@involt.io>
2021-12-26 17:24:34 +0100

    Create a generic tabs component

---
+++ src/ui/components/Tabs.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+interface Props {
+  tabs: string;
+  active: string;
+  onClick: any
+};
+
+export default class Tabs extends React.Component<Props> {
+
+  render() {
+    return(
+      <div className='tabs'>
+        {this.props.tabs.split(' ').map(tab =>
+          <div key={tab.toLowerCase()} className={`tabs__tab type ${this.props.active === tab ? 'tabs__tab--active' : ''}`} onClick={this.props.onClick}>{tab}</div>
+        )}
+      </div>
+    )
+  }
+
+}
