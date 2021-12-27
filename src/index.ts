ade301b6429e4c99e72375409b66b8090fc88227
Aurélien Grimaud <a.grimaud@involt.io>
2021-12-27 20:17:40 +0100

    Make a function to create a palette frame

--- src/index.ts
+++ src/index.ts
@@ -17,53 +17,10 @@ figma.ui.onmessage = msg => {
   if (msg.type === 'create-palette' && figma.currentPage.selection.length != 0) {
 
     const scene: SceneNode[] = [],
-          palette: FrameNode = figma.createFrame();
+          palette: any = createPalette(msg.lightness);
 
-    palette.layoutMode = "VERTICAL";
-    palette.primaryAxisSizingMode = "AUTO";
-    palette.counterAxisSizingMode = "AUTO";
-    palette.name = "Awesome Palette";
-    palette.paddingTop = palette.paddingRight = palette.paddingBottom = palette.paddingLeft = 32;
-    palette.cornerRadius = 16;
-    palette.setPluginData('min', msg.lightness.min.toString());
-    palette.setPluginData('max', msg.lightness.max.toString());
-    palette.setPluginData('scale', JSON.stringify(msg.lightness.scale));
-
-    figma.currentPage.selection.forEach(element => {
-
-      let fills = element['fills'].filter(fill => fill.type === "SOLID");
-
-      if (fills.length != 0) {
-
-        fills.forEach(fill => {
-
-          let rgb = fill.color;
-
-          const paletteItem: FrameNode = figma.createFrame();
-          paletteItem.layoutMode = "HORIZONTAL";
-          paletteItem.counterAxisSizingMode = "AUTO";
-          paletteItem.name = element.name;
-
-          Object.values(msg.lightness.scale).forEach(lightness => {
-      			let newColor = chroma([rgb.r * 255, rgb.g * 255, rgb.b * 255]).set('lch.l', lightness);
-            const sample = new Sample(`${element.name}-${Object.keys(msg.lightness.scale).find(key => msg.lightness.scale[key] === lightness).substr(10)}`, 128, 96, newColor._rgb).makeNode();
-            paletteItem.name = element.name;
-            paletteItem.appendChild(sample)
-      		});
-
-          palette.appendChild(paletteItem)
-          figma.currentPage.appendChild(palette);
-          scene.push(palette);
-
-        })
-
-      } else {
-        figma.notify(`The layer "${element.name}" must get at least one solid color`)
-      }
-
-
-    });
-
+    figma.currentPage.appendChild(palette);
+    scene.push(palette);
     figma.currentPage.selection = scene;
     figma.viewport.scrollAndZoomIntoView(scene)
 
@@ -71,4 +28,53 @@ figma.ui.onmessage = msg => {
     figma.notify('Select some filled layers to generate a palette')
   };
 
+
+};
+
+const createPalette = (data) => {
+  const palette: FrameNode = figma.createFrame();
+
+  palette.layoutMode = "VERTICAL";
+  palette.primaryAxisSizingMode = "AUTO";
+  palette.counterAxisSizingMode = "AUTO";
+  palette.name = "Awesome Palette";
+  palette.paddingTop = palette.paddingRight = palette.paddingBottom = palette.paddingLeft = 32;
+  palette.cornerRadius = 16;
+  palette.setPluginData('min', data.min.toString());
+  palette.setPluginData('max', data.max.toString());
+  palette.setPluginData('scale', JSON.stringify(data.scale));
+
+  figma.currentPage.selection.forEach(element => {
+
+    let fills = element['fills'].filter(fill => fill.type === "SOLID");
+
+    if (fills.length != 0) {
+
+      fills.forEach(fill => {
+
+        let rgb = fill.color;
+
+        const paletteItem: FrameNode = figma.createFrame();
+        paletteItem.layoutMode = "HORIZONTAL";
+        paletteItem.counterAxisSizingMode = "AUTO";
+        paletteItem.name = element.name;
+
+        Object.values(data.scale).forEach(lightness => {
+          let newColor = chroma([rgb.r * 255, rgb.g * 255, rgb.b * 255]).set('lch.l', lightness);
+          const sample = new Sample(`${element.name}-${Object.keys(data.scale).find(key => data.scale[key] === lightness).substr(10)}`, 128, 96, newColor._rgb).makeNode();
+          paletteItem.name = element.name;
+          paletteItem.appendChild(sample)
+        });
+
+        palette.appendChild(paletteItem)
+        return palette
+
+      })
+
+    } else {
+      figma.notify(`The layer "${element.name}" must get at least one solid color`);
+      return
+    }
+
+  })
 }
