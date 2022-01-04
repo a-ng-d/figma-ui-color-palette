873fa23cf0fedc9a5585982e8505a2377e491854
Aurélien Grimaud <a.grimaud@involt.io>
2022-01-04 23:45:31 +0100

    Change app to App

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env, argv) => ({
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
   entry: {
-    ui: './src/ui/app.tsx', // The entry point for your UI code
+    ui: './src/ui/App.tsx', // The entry point for your UI code
     code: './src/index.ts', // The entry point for your plugin code
   },
 
