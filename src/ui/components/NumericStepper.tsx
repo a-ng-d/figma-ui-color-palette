e45d6e567036f9316e01b2f0dcaaf3e9478e0668
Aurélien Grimaud <a.grimaud@involt.io>
2022-01-08 12:16:27 +0100

    Remove the id property from the numeric stepper component

--- src/ui/components/NumericStepper.tsx
+++ src/ui/components/NumericStepper.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 interface Props {
-  id: string;
   value: string;
   min: string;
   max: string;
@@ -14,7 +13,7 @@ export default class NumericStepper extends React.Component<Props> {
   render() {
     return(
       <div className='input'>
-        <input type='number' min={this.props.min} max={this.props.max} step={this.props.step} className='input__field' value={this.props.value} onChange={this.props.onChange} />
+        <input type='number' className='input__field' min={this.props.min} max={this.props.max} step={this.props.step} value={this.props.value} onChange={this.props.onChange} />
       </div>
     )
   }
