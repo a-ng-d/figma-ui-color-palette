e5065da204001333c1e587867ced39497b7d22ec
Aurélien Grimaud <a.grimaud@involt.io>
2022-01-13 19:10:42 +0100

    Tag the palette as corrupted in case of editing

--- src/index.ts
+++ src/index.ts
@@ -18,65 +18,65 @@ figma.ui.onmessage = msg => {
   switch (msg.type) {
 
     case 'create-palette':
-      if (figma.currentPage.selection.length != 0) {
+      const scene: SceneNode[] = [];
 
-        const scene: SceneNode[] = [];
+      palette = new Palette(
+        msg.palette.min,
+        msg.palette.max,
+        msg.palette.scale,
+        msg.palette.captions
+      ).makeNode();
 
-        palette = new Palette(
-            msg.palette.min,
-            msg.palette.max,
-            msg.palette.scale,
-            msg.palette.captions
-          ).makeNode();
-
-        if (palette.children.length != 0) {
-          figma.currentPage.appendChild(palette);
-          scene.push(palette);
-          figma.currentPage.selection = scene;
-          figma.viewport.scrollAndZoomIntoView(scene)
-        } else {
-          palette.remove()
-        }
-
-      } else {
-        figma.notify('Select some filled layers to create a palette')
-      };
+      if (palette.children.length != 0) {
+        figma.currentPage.appendChild(palette);
+        scene.push(palette);
+        figma.currentPage.selection = scene;
+        figma.viewport.scrollAndZoomIntoView(scene)
+      } else
+        palette.remove()
       break;
 
     case 'edit-palette':
       palette = figma.currentPage.selection[0];
-      palette.setPluginData('min', msg.palette.min.toString());
-      palette.setPluginData('max', msg.palette.max.toString());
-      palette.setPluginData('scale', JSON.stringify(msg.palette.scale));
+      if (palette.children.length == 1) {
+        palette.setPluginData('min', msg.palette.min.toString());
+        palette.setPluginData('max', msg.palette.max.toString());
+        palette.setPluginData('scale', JSON.stringify(msg.palette.scale));
 
-      palette.children[0].remove();
-      palette.appendChild(new Colors({
-        colors: JSON.parse(palette.getPluginData('colors')),
-        scale: JSON.parse(palette.getPluginData('scale')),
-        captions: palette.getPluginData('captions') == 'hasCaptions' ? true : false
-      }).makeNode());
+        palette.children[0].remove();
+        palette.appendChild(new Colors({
+          colors: JSON.parse(palette.getPluginData('colors')),
+          scale: JSON.parse(palette.getPluginData('scale')),
+          captions: palette.getPluginData('captions') == 'hasCaptions' ? true : false
+        }).makeNode());
 
-      figma.ui.postMessage(palette.getPluginData('scale'));
+        figma.ui.postMessage(palette.getPluginData('scale'));
+      } else
+        figma.notify('Your Awesome Color Palette seems corrupted. Do not edit any layer within it.')
       break;
 
     case 'update-captions':
       palette = figma.currentPage.selection[0];
-
-      if (msg.palette.captions) {
-        palette.setPluginData('captions', 'hasCaptions');
-        palette.children[0].children.forEach(row => {
-          row.children.forEach(sample => {
-            sample.children[0].visible = true
-          })
-        })
-      } else {
-        palette.setPluginData('captions', 'hasNotCaptions');
-        palette.children[0].children.forEach(row => {
-          row.children.forEach(sample => {
-            sample.children[0].visible = false
-          })
-        })
-      }
+      if (palette.children.length == 1) {
+        if (msg.palette.captions) {
+          palette.setPluginData('captions', 'hasCaptions');
+          palette.children[0].remove();
+          palette.appendChild(new Colors({
+            colors: JSON.parse(palette.getPluginData('colors')),
+            scale: JSON.parse(palette.getPluginData('scale')),
+            captions: palette.getPluginData('captions') == 'hasCaptions' ? true : false
+          }).makeNode());
+        } else {
+          palette.setPluginData('captions', 'hasNotCaptions');
+          palette.children[0].remove();
+          palette.appendChild(new Colors({
+            colors: JSON.parse(palette.getPluginData('colors')),
+            scale: JSON.parse(palette.getPluginData('scale')),
+            captions: palette.getPluginData('captions') == 'hasCaptions' ? true : false
+          }).makeNode());
+        }
+      } else
+        figma.notify('Your Awesome Color Palette seems corrupted. Do not edit any layer within it.')
       break;
 
     case 'get-infos':
@@ -93,43 +93,49 @@ figma.ui.onmessage = msg => {
     case 'create-local-styles':
       palette = figma.currentPage.selection[0];
       i = 0;
-
-      palette.children.forEach(row => {
-        row.children.forEach(sample => {
-          const style = new Style(
-            sample.name.replace('-', '/'),
-            sample.fills[0].color
-          ).makeNode();
-          figma.moveLocalPaintStyleAfter(style, null);
-          i++
+      if (palette.children.length == 1) {
+        palette.children.forEach(row => {
+          row.children.forEach(sample => {
+            const style = new Style(
+              sample.name.replace('-', '/'),
+              sample.fills[0].color
+            ).makeNode();
+            figma.moveLocalPaintStyleAfter(style, null);
+            i++
+          })
         })
-      })
-      figma.notify(`${i} local color styles have been created 🙌`)
+        figma.notify(`${i} local color styles have been created 🙌`)
+      } else
+        figma.notify('Your Awesome Color Palette seems corrupted. Do not edit any layer within it.')
       break;
 
     case 'update-local-styles':
       palette = figma.currentPage.selection[0];
-      const localStyles = figma.getLocalPaintStyles();
-      i = 0;
 
-      palette.children.forEach(row => {
-        row.children.forEach(sample => {
-          localStyles.forEach(localStyle => {
-            if (sample.name === localStyle.name.replace('/', '-')) {
-              if (JSON.stringify(localStyle.paints[0]['color']) != JSON.stringify(sample.fills[0]['color'])) {
-                localStyle.paints = sample.fills;
-                i++
+      if (palette.children.length == 1) {
+        const localStyles = figma.getLocalPaintStyles();
+        i = 0;
+
+        palette.children.forEach(row => {
+          row.children.forEach(sample => {
+            localStyles.forEach(localStyle => {
+              if (sample.name === localStyle.name.replace('/', '-')) {
+                if (JSON.stringify(localStyle.paints[0]['color']) != JSON.stringify(sample.fills[0]['color'])) {
+                  localStyle.paints = sample.fills;
+                  i++
+                }
               }
-            }
+            })
           })
-        })
-      });
-      if (i > 1)
-        figma.notify(`${i} local color styles have been updated 🙌`)
-      else if (i == 1)
-        figma.notify(`${i} local color style has been updated 🙌`)
-      else
-        figma.notify(`No local color style has been updated`)
+        });
+        if (i > 1)
+          figma.notify(`${i} local color styles have been updated 🙌`)
+        else if (i == 1)
+          figma.notify(`${i} local color style has been updated 🙌`)
+        else
+          figma.notify(`No local color style has been updated`)
+      } else
+      figma.notify('Your Awesome Color Palette seems corrupted. Do not edit any layer within it.')
 
   }
 
