34cdc14376c28e8f6c5831e99a59c262517a500c
Aurélien Grimaud <a.grimaud@involt.io>
2022-02-15 01:43:40 +0100

    Create a new type of caption to make a row name

--- src/modules/Caption.ts
+++ src/modules/Caption.ts
@@ -2,21 +2,23 @@ import chroma from 'chroma-js';
 
 export default class Caption {
 
-  scale: string;
+  name: string;
   rgb: Array<number>;
   hex: string;
   lch: Array<number>;
-  nodeTitle: TextNode;
+  nodeScale: TextNode;
   nodeProperties: TextNode;
+  nodeName: TextNode;
   node: FrameNode;
 
-  constructor(scale, rgb) {
-    this.scale = scale;
+  constructor(name, rgb) {
+    this.name = name;
     this.rgb = rgb;
     this.hex = chroma(rgb).hex();
     this.lch = chroma(rgb).lch();
-    this.nodeTitle = figma.createText();
+    this.nodeScale = figma.createText();
     this.nodeProperties = figma.createText();
+    this.nodeName = figma.createText();
     this.node = figma.createFrame()
   }
 
@@ -38,15 +40,15 @@ export default class Caption {
     return `${this.hex.toUpperCase()}\nR ${Math.floor(this.rgb[0])}﹒G ${Math.floor(this.rgb[1])}﹒B ${Math.floor(this.rgb[2])}\nL ${Math.floor(this.lch[0])}﹒C ${Math.floor(this.lch[1])}﹒H ${Math.floor(this.lch[2])}\n${this.getLevel()}﹒${this.getContrast().toFixed(2)} : 1`
   }
 
-  makeNodeTitle() {
-    this.nodeTitle.name = 'lightning-scale';
-    this.nodeTitle.characters = this.scale;
-    this.nodeTitle.fontName = {
+  makeName() {
+    this.nodeName.name = 'color-name';
+    this.nodeName.characters = this.name;
+    this.nodeName.fontName = {
       family: 'Roboto Mono',
       style: 'Medium'
     };
-    this.nodeTitle.fontSize = 10;
-    this.nodeTitle.fills = [{
+    this.nodeName.fontSize = 10;
+    this.nodeName.fills = [{
       type: 'SOLID',
       color: {
         r: this.getCaptionColor()[0],
@@ -54,9 +56,31 @@ export default class Caption {
         b: this.getCaptionColor()[2]
       }
     }];
-    this.nodeTitle.layoutAlign = 'STRETCH';
+    this.nodeName.layoutAlign = 'STRETCH';
+    this.nodeName.layoutGrow = 1;
 
-    return this.nodeTitle
+    return this.nodeName
+  }
+
+  makeNodeScale() {
+    this.nodeScale.name = 'lightness-scale';
+    this.nodeScale.characters = this.name;
+    this.nodeScale.fontName = {
+      family: 'Roboto Mono',
+      style: 'Medium'
+    };
+    this.nodeScale.fontSize = 10;
+    this.nodeScale.fills = [{
+      type: 'SOLID',
+      color: {
+        r: this.getCaptionColor()[0],
+        g: this.getCaptionColor()[1],
+        b: this.getCaptionColor()[2]
+      }
+    }];
+    this.nodeScale.layoutAlign = 'STRETCH';
+
+    return this.nodeScale
   }
 
   makeNodeProperties() {
@@ -80,7 +104,7 @@ export default class Caption {
     return this.nodeProperties
   }
 
-  makeNode() {
+  makeNode(type) {
     this.node.name = 'captions';
     this.node.fills = [];
     this.node.layoutMode = 'VERTICAL';
@@ -91,8 +115,15 @@ export default class Caption {
     this.node.layoutGrow = 1;
     this.node.locked = true;
 
-    this.node.appendChild(this.makeNodeTitle());
-    this.node.appendChild(this.makeNodeProperties());
+    if (type === 'SAMPLE') {
+      this.nodeName.remove();
+      this.node.appendChild(this.makeNodeScale());
+      this.node.appendChild(this.makeNodeProperties());
+    } else if (type === 'NAME') {
+      this.nodeScale.remove();
+      this.nodeProperties.remove();
+      this.node.appendChild(this.makeName())
+    };
 
     return this.node
   }
