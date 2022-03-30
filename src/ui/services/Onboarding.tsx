fe3d7f5fe58f18498022a64d5b546c6a1dfa73f4
Aurélien Grimaud <a.grimaud@involt.io>
2022-03-30 22:31:42 +0200

    Add an onboarding service when nothing is selected

---
+++ src/ui/services/Onboarding.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Message from '../components/Message';
+
+export default class Onboarding extends React.Component {
+
+  render() {
+    return (
+      <section>
+      <div className='onboarding'>
+        <Message
+          icon='list-tile'
+          messages= {[
+            'Select your starting colors on the Figma canvas to create a UI Color Palette',
+            'From the layers filled with a solid color'
+          ]}
+        />
+        <div className='label'>or</div>
+        <Message
+          icon='theme'
+          messages= {[
+            'Select an UI Color Palette to edit it'
+          ]}
+        />
+      </div>
+      </section>
+    )
+  }
+
+}
