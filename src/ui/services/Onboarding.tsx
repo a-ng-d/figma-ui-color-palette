c88c356b567032ae4051faa1ef2e117b84d9cf78
Aurélien Grimaud <a.grimaud@involt.io>
2022-04-01 01:16:07 +0200

    Add the plugin icon on the onboarding message

--- src/ui/services/Onboarding.tsx
+++ src/ui/services/Onboarding.tsx
@@ -6,22 +6,22 @@ export default class Onboarding extends React.Component {
   render() {
     return (
       <section>
-      <div className='onboarding'>
-        <Message
-          icon='list-tile'
-          messages= {[
-            'Select your starting colors on the Figma canvas to create a UI Color Palette',
-            'From the layers filled with a solid color'
-          ]}
-        />
-        <div className='label'>or</div>
-        <Message
-          icon='theme'
-          messages= {[
-            'Select an UI Color Palette to edit it'
-          ]}
-        />
-      </div>
+        <div className='onboarding'>
+          <div className='plugin-icon'></div>
+          <Message
+            icon='list-tile'
+            messages= {[
+              'Select your starting colors on the Figma canvas to create a UI Color Palette (from the layers filled with a solid color)',
+            ]}
+          />
+          <div className='label'>or</div>
+          <Message
+            icon='theme'
+            messages= {[
+              'Select an UI Color Palette to edit it'
+            ]}
+          />
+        </div>
       </section>
     )
   }
