7127ba5457dc7d4d63220389978c9aea377e86bb
Aurélien Grimaud <a.grimaud@involt.io>
2022-10-18 11:01:07 +0200

    Get an empty space between right and left options to mouse down the parent

--- src/ui/components/ColorItem.tsx
+++ src/ui/components/ColorItem.tsx
@@ -17,62 +17,66 @@ export default class ColorItem extends React.Component<Props> {
 
   render() {
     return(
-      <li id={this.props.name} data-id={this.props.uuid} className='colors__item'>
-        <Input
-          type='text'
-          id='rename'
-          icon={{type: 'none', value: null}}
-          value={this.props.name}
-          min=''
-          max=''
-          onChange={this.inputHandler}
-        />
-        <div className='colors__parameters'>
+      <li id={this.props.name} data-id={this.props.uuid} className='colors__item' onMouseDown={this.activeReorder}>
+        <div className="colors__left-options">
           <Input
-            type='color'
-            id='hex'
+            type='text'
+            id='rename'
             icon={{type: 'none', value: null}}
-            value={this.props.hex}
+            value={this.props.name}
             min=''
             max=''
             onChange={this.inputHandler}
           />
-          <Input
-            type='number'
-            id='lightness'
-            icon={{type: 'letter', value: 'L'}}
-            value={chroma(this.props.hex).lch()[0].toFixed(0)}
-            min='0'
-            max='100'
-            onChange={this.inputHandler}
-          />
-          <Input
-            type='number'
-            id='chroma'
-            icon={{type: 'letter', value: 'C'}}
-            value={chroma(this.props.hex).lch()[1].toFixed(0)}
-            min='0'
-            max='100'
-            onChange={this.inputHandler}
-          />
-          <Input
-            type='number'
-            id='hue'
-            icon={{type: 'letter', value: 'H'}}
-            value={chroma(this.props.hex).lch()[2].toFixed(0) == 'NaN' ? 0 : chroma(this.props.hex).lch()[2].toFixed(0)}
-            min='0'
-            max='360'
-            onChange={this.inputHandler}
+          <div className='colors__parameters'>
+            <Input
+              type='color'
+              id='hex'
+              icon={{type: 'none', value: null}}
+              value={this.props.hex}
+              min=''
+              max=''
+              onChange={this.inputHandler}
+            />
+            <Input
+              type='number'
+              id='lightness'
+              icon={{type: 'letter', value: 'L'}}
+              value={chroma(this.props.hex).lch()[0].toFixed(0)}
+              min='0'
+              max='100'
+              onChange={this.inputHandler}
+            />
+            <Input
+              type='number'
+              id='chroma'
+              icon={{type: 'letter', value: 'C'}}
+              value={chroma(this.props.hex).lch()[1].toFixed(0)}
+              min='0'
+              max='100'
+              onChange={this.inputHandler}
+            />
+            <Input
+              type='number'
+              id='hue'
+              icon={{type: 'letter', value: 'H'}}
+              value={chroma(this.props.hex).lch()[2].toFixed(0) == 'NaN' ? 0 : chroma(this.props.hex).lch()[2].toFixed(0)}
+              min='0'
+              max='360'
+              onChange={this.inputHandler}
+            />
+          </div>
+        </div>
+        <div className="colors__right-options">
+          <Button
+            id='remove'
+            icon='minus'
+            type='icon'
+            label={null}
+            state=''
+            action={this.inputHandler}
           />
         </div>
-        <Button
-          id='remove'
-          icon='minus'
-          type='icon'
-          label={null}
-          state=''
-          action={this.inputHandler}
-        />
       </li>
     )
   }
