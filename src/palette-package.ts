9f3693dcee9ecab5c86fda9b4d8ae5fdec5e02fb
Aurélien Grimaud <a.grimaud@involt.io>
2022-10-21 01:11:57 +0200

    Type the palette and presets objects

--- src/palette-package.ts
+++ src/palette-package.ts
@@ -1,4 +1,23 @@
-export const palette = {
+interface Preset {
+  name: string,
+  scale: Array<number>,
+  min: number,
+  max: number
+}
+
+interface Scale {
+  [key: string]: string
+}
+
+interface Palette {
+  scale: Scale,
+  min: number,
+  max: number,
+  captions: boolean,
+  preset: {}
+}
+
+export const palette: Palette = {
   scale: {},
   min: null,
   max: null,
@@ -12,23 +31,23 @@ export const presets = {
     scale: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
     min: 24,
     max: 96
-  },
+  } as Preset,
   ant: {
     name: 'Ant Design (1-13)',
     scale: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
     min: 16,
     max: 100
-  },
+  } as Preset,
   atlassian: {
     name: 'Atlassian (0-900)',
     scale: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900],
     min: 8,
     max: 100
-  },
+  } as Preset,
   custom: {
     name: 'Custom',
     scale: [1, 2],
     min: 0,
     max: 100
-  }
+  } as Preset
 }
