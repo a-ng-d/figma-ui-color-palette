7f9829a34a56e1af0da49a1f551e6b475da4ab46
Aurélien Grimaud <a.grimaud@involt.io>
2022-10-23 23:14:56 +0200

    Do not duplicate local styles at creating if they already exist

--- src/index.ts
+++ src/index.ts
@@ -107,22 +107,33 @@ figma.ui.onmessage = msg => {
 
     case 'create-local-styles':
       palette = figma.currentPage.selection[0];
-      i = 0;
+
       if (palette.children.length == 1) {
+        const localStyles = figma.getLocalPaintStyles();
+        i = 0;
+
         palette.children[0].children.forEach(row => {
           if (row.name != '_header' && row.name != '_title')
             row.children.forEach((sample, index) => {
               if (index != 0) {
-                const style = new Style(
-                  `${row.name}/${sample.name.replace(row.name + '-', '')}`,
-                  sample.fills[0].color
-                ).makeNode();
-                figma.moveLocalPaintStyleAfter(style, null);
-                i++
+                console.log(localStyles.filter(e => e.name === row.name).length)
+                if (localStyles.filter(e => e.name === `${row.name}/${sample.name.replace(row.name + '-', '')}`).length == 0) {
+                  const style = new Style(
+                    `${row.name}/${sample.name.replace(row.name + '-', '')}`,
+                    sample.fills[0].color
+                  ).makeNode();
+                  figma.moveLocalPaintStyleAfter(style, null);
+                  i++
+                }
               }
             })
         })
-        figma.notify(`${i} local color styles have been created 🙌`)
+        if (i > 1)
+          figma.notify(`${i} local color styles have been created 🙌`)
+        else if (i == 1)
+          figma.notify(`${i} local color style has been created 🙌`)
+        else
+          figma.notify(`No local color style has been created 🙌`)
       } else
         figma.notify('Your UI Color Palette seems corrupted. Do not edit any layer within it.')
       break;
