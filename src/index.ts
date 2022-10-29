8ceb947fb319aa590d03885a2654e70a54fa477c
Aurélien Grimaud <a.grimaud@involt.io>
2022-10-30 01:55:20 +0200

    Turn messageToUI into a module

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import Palette from './canvas/Palette';
 import Style from './canvas/Style';
 import Colors from './canvas/Colors';
+import { messageToUI } from './bridges/messageToUI';
 import { presets } from './utils/palette-package';
-import { setData } from './utils/setData';
 
 figma.showUI(__html__);
 figma.ui.resize(640, 320);
@@ -167,43 +167,4 @@ figma.ui.onmessage = msg => {
 
   }
 
-};
-
-const messageToUI = () => {
-  const selection: ReadonlyArray<BaseNode> = figma.currentPage.selection
-
-  if (selection.length == 1 && selection[0].getPluginData('scale') != '') {
-    if (selection[0].getPluginData('preset') === '')
-      selection[0].setPluginData('preset', JSON.stringify(presets.material));
-
-    if (!selection[0].getPluginData('colors').includes('oklch'))
-      selection[0].setPluginData('colors', setData(selection[0].getPluginData('colors'), 'oklch', false));
-
-    if (!selection[0].getPluginData('colors').includes('hueShifting'))
-      selection[0].setPluginData('colors', setData(selection[0].getPluginData('colors'), 'hueShifting', 0));
-
-    figma.ui.postMessage({
-      type: 'palette-selected',
-      data: {
-        scale: JSON.parse(selection[0].getPluginData('scale')),
-        captions: selection[0].getPluginData('captions'),
-        colors: JSON.parse(selection[0].getPluginData('colors')),
-        preset: JSON.parse(selection[0].getPluginData('preset'))
-      }
-    })
-  }
-  else if (selection.length == 0)
-    figma.ui.postMessage({
-      type: 'empty-selection',
-      data: {}
-    });
-
-  selection.forEach(element => {
-    if (element.type != 'GROUP')
-      if (element['fills'].filter(fill => fill.type === 'SOLID').length != 0 && element.getPluginData('scale') === '')
-        figma.ui.postMessage({
-          type: 'color-selected',
-          data: {}
-        })
-  })
 };
