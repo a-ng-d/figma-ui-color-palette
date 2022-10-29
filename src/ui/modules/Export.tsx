48d56287e9d68528d02b1b20c8fc9b8fd0d5eef7
Aurélien Grimaud <a.grimaud@involt.io>
2022-10-30 00:25:19 +0200

    Create a section to export the palette

---
+++ src/ui/modules/Export.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface Props {
+
+};
+
+export default class Export extends React.Component<Props> {
+
+  render() {
+    return (
+      <></>
+    )
+  }
+
+}
