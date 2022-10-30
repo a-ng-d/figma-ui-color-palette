001aea9e1b390cf3c8ab1c2ff89a8e2ad1662640
Aurélien Grimaud <a.grimaud@involt.io>
2022-10-30 02:10:30 +0200

    Turn updateLocalColors into a module

---
+++ src/bridges/updateLocalStyles.ts
@@ -0,0 +1,30 @@
+export const updateLocalStyles = (palette, i) => {
+
+  palette = figma.currentPage.selection[0];
+
+  if (palette.children.length == 1) {
+    const localStyles = figma.getLocalPaintStyles();
+    i = 0;
+
+    palette.children[0].children.forEach(row => {
+      row.children.forEach(sample => {
+        localStyles.forEach(localStyle => {
+          if (`${row.name}/${sample.name}` === localStyle.name) {
+            if (JSON.stringify(localStyle.paints[0]['color']) != JSON.stringify(sample.fills[0]['color'])) {
+              localStyle.paints = sample.fills;
+              i++
+            }
+          }
+        })
+      })
+    });
+    if (i > 1)
+      figma.notify(`${i} local color styles have been updated 🙌`)
+    else if (i == 1)
+      figma.notify(`${i} local color style has been updated 🙌`)
+    else
+      figma.notify(`No local color style has been updated`)
+  } else
+    figma.notify('Your UI Color Palette seems corrupted. Do not edit any layer within it.')
+
+}
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { updateScale } from './bridges/updateScale';
 import { updateCaptions } from './bridges/updateCaptions';
 import { updateColors } from './bridges/updateColors';
 import { createLocalStyles } from './bridges/createLocalStyles';
+import { updateLocalStyles } from './bridges/updateLocalStyles';
 import { messageToUI } from './bridges/messageToUI';
 import { presets } from './utils/palette-package';
 
@@ -36,33 +37,7 @@ figma.ui.onmessage = msg => {
 
     case 'create-local-styles': createLocalStyles(palette, i); break;
 
-    case 'update-local-styles':
-      palette = figma.currentPage.selection[0];
-
-      if (palette.children.length == 1) {
-        const localStyles = figma.getLocalPaintStyles();
-        i = 0;
-
-        palette.children[0].children.forEach(row => {
-          row.children.forEach(sample => {
-            localStyles.forEach(localStyle => {
-              if (`${row.name}/${sample.name}` === localStyle.name) {
-                if (JSON.stringify(localStyle.paints[0]['color']) != JSON.stringify(sample.fills[0]['color'])) {
-                  localStyle.paints = sample.fills;
-                  i++
-                }
-              }
-            })
-          })
-        });
-        if (i > 1)
-          figma.notify(`${i} local color styles have been updated 🙌`)
-        else if (i == 1)
-          figma.notify(`${i} local color style has been updated 🙌`)
-        else
-          figma.notify(`No local color style has been updated`)
-      } else
-        figma.notify('Your UI Color Palette seems corrupted. Do not edit any layer within it.')
+    case 'update-local-styles': updateLocalStyles(palette, i); break;
 
   }
 
