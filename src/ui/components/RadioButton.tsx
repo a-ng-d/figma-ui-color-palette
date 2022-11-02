2e65f967dba5e6e6db0a6168b22e68c121c96438
Aurélien Grimaud <a.grimaud@involt.io>
2022-11-02 20:31:43 +0100

    Add a Radio button component

---
+++ src/ui/components/RadioButton.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+interface Props {
+  id: string;
+  label: string;
+  isChecked: boolean;
+  isDisabled: boolean;
+  feature: string;
+  onChange: any
+};
+
+export default class RadioButton extends React.Component<Props> {
+
+  render() {
+    return(
+      <div className='radio'>
+        <input
+          data-feature={this.props.feature}
+          id={this.props.id}
+          className='radio__button'
+          type='radio'
+          checked={this.props.isChecked}
+          disabled={this.props.isDisabled}
+          onChange={this.props.onChange}
+          name='radioGroup'
+        />
+        <label className='radio__label' htmlFor={this.props.id}>{this.props.label}</label>
+      </div>
+    )
+  }
+
+}
