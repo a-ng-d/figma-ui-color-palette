72fef11f7a7b034102260fb07b410262f58bbd05
Aurélien Grimaud <a.grimaud@involt.io>
2022-12-02 17:46:27 +0100

    Get two types of message

--- src/ui/components/Message.tsx
+++ src/ui/components/Message.tsx
@@ -7,23 +7,35 @@ interface Props {
 
 export default class Message extends React.Component<Props> {
 
-  render() {
-    return(
+  SingleMessage = () => {
+    return (
       <div className='onboarding-tip'>
         <div className={`icon icon--${this.props.icon}`}></div>
-        {this.props.messages.map((message, index) => {
-          if (index > 0) {
-            return(
-              <React.Fragment key={`message-${index}`}>
-                <div className="onboarding-tip__separator"></div>
-                <div className='onboarding-tip__msg'>{message}</div>
-              </React.Fragment>
-            )
-          } else {
-            return <div className='onboarding-tip__msg' key={`message-${index}`}>{message}</div>
-          }
-        })}
+        <div className='onboarding-tip__msg'>{this.props.messages[0]}</div>
       </div>
+    )
+  }
+
+  MultipleMessages = () => {
+    return (
+      <div className='callout'>
+        <div>
+          <div className='onboarding-tip'>
+            <div className={`icon icon--${this.props.icon}`}></div>
+            {this.props.messages.map((message, index) => {
+              return <div className='onboarding-tip__msg' key={`message-${index}`}>{message}</div>
+            })}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <>
+      {this.props.messages.length > 1 ? <this.MultipleMessages /> : <this.SingleMessage />}
+      </>
     )
   }
 
