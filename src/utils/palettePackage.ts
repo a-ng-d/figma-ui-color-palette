310e4045e2857fc867f134e2c240fb5685d0d246
Aurélien Grimaud <a.grimaud@involt.io>
2022-12-07 12:47:42 +0100

    Review presets name

--- src/utils/palettePackage.ts
+++ src/utils/palettePackage.ts
@@ -27,37 +27,37 @@ export const palette: Palette = {
 
 export const presets = {
   material: {
-    name: 'Material Design﹒Google (50-900)',
+    name: 'Material Design 50-900 (Google) ',
     scale: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
     min: 24,
     max: 96
   } as Preset,
   ant: {
-    name: 'Ant Design (1-10)',
+    name: 'Ant Design 1-10',
     scale: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     min: 24,
     max: 96
   } as Preset,
   atlassian: {
-    name: 'ADS Foundations﹒Atlassian (50-500)',
+    name: 'ADS Foundations 50-500 (Atlassian)',
     scale: [50, 75, 100, 200, 300, 400, 500],
     min: 24,
     max: 96
   } as Preset,
   atlassianNeutral: {
-    name: 'ADS Foundations﹒Neutral﹒Atlassian (50-500)',
+    name: 'ADS Foundations, Neutral 50-500 (Atlassian)',
     scale: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900],
     min: 8,
     max: 100
   } as Preset,
   carbon: {
-    name: 'Carbon﹒IBM (10-100)',
+    name: 'Carbon 10-100 (IBM)',
     scale: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
     min: 24,
     max: 96
   } as Preset,
   base: {
-    name: 'Base﹒Uber (50-700)',
+    name: 'Base 50-700 (Uber)',
     scale: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
     min: 24,
     max: 96
