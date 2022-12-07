3aee89fa1597ff10a5afc5a65b26d8a8e3bf8e12
Aurélien Grimaud <a.grimaud@involt.io>
2022-12-07 23:02:40 +0100

    Insert a first form item for driving the palette name

--- src/ui/modules/Settings.tsx
+++ src/ui/modules/Settings.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import FormItem from './../components/FormItem';
+import Input from './../components/Input';
 
 interface Props {
 
@@ -9,7 +11,28 @@ export default class Settings extends React.Component<Props> {
   render() {
     return (
       <div className='settings controls__control'>
-
+        <div className='settings__group'>
+          <div className='section-controls'>
+            <div className='section-title'>Base information</div>
+          </div>
+          <div className='settings__item'>
+            <FormItem
+              label='Palette name'
+              id='rename-palette'
+            >
+              <Input
+                type='text'
+                icon={{type: 'none', value: null}}
+                value={'test'}
+                min=''
+                max=''
+                feature='rename-palette'
+                onChange={null}
+                onFocus={null}
+              />
+            </FormItem>
+          </div>
+        </div>
       </div>
     )
   }
