ddbe091a9e998fff7bb3061890d7e2b4d26dca58
Aurélien Grimaud <a.grimaud@involt.io>
2022-12-08 17:55:28 +0100

    Update the palette global object when renamed the palette

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -316,6 +316,7 @@ class App extends React.Component {
     switch (e.target.dataset.feature) {
 
       case 'rename-palette':
+        palette.name = e.target.value,
         this.setState({
           paletteName: e.target.value,
           onGoingStep: 'settings changed'
