a1771b0e5a9f853559caaf53b57eb2f5a34b476f
Aurélien Grimaud <a.grimaud@involt.io>
2022-12-08 18:00:24 +0100

    Update the palette name whatever the kind of bridge

--- src/bridges/updateColors.ts
+++ src/bridges/updateColors.ts
@@ -3,18 +3,33 @@ import Colors from './../canvas/Colors';
 const updateColors = (msg, palette) => {
 
   palette = figma.currentPage.selection[0];
-  palette.setPluginData('colors', JSON.stringify(msg.data));
-  palette.children[0].remove();
-  palette.appendChild(new Colors({
-    colors: JSON.parse(palette.getPluginData('colors')),
-    scale: JSON.parse(palette.getPluginData('scale')),
-    captions: palette.getPluginData('captions') == 'hasCaptions' ? true : false,
-    preset: JSON.parse(palette.getPluginData('preset'))
-  }).makeNode());
+  
+  if (palette.children.length == 1) {
+    const paletteName: string = palette.getPluginData('name') === '' ? 'UI Color Palette' : palette.getPluginData('name'),
+          scale: string = JSON.parse(palette.getPluginData('scale')),
+          captions: boolean = palette.getPluginData('captions') == 'hasCaptions' ? true : false,
+          preset = JSON.parse(palette.getPluginData('preset'));
+    
+    let colors: string; 
 
-  // palette migration
-  palette.counterAxisSizingMode = 'AUTO';
-  palette.name = `UI Color Palette﹒${JSON.parse(palette.getPluginData('preset')).name}`
+    palette.setPluginData('colors', JSON.stringify(msg.data));
+    colors = JSON.parse(palette.getPluginData('colors'));
+
+    palette.children[0].remove();
+    palette.appendChild(new Colors({
+      paletteName: paletteName,
+      colors: colors,
+      scale: scale,
+      captions: captions,
+      preset: preset
+    }).makeNode());
+
+    // palette migration
+    palette.counterAxisSizingMode = 'AUTO';
+    palette.name = `${paletteName}﹒${preset.name}`
+  } else
+    figma.notify('Your UI Color Palette seems corrupted. Do not edit any layer within it.')
+
 
 };
 
