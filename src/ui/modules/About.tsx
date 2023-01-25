6d1db89e94fb60d049bc6b902733857521a603fa
Aurélien Grimaud <a.grimaud@involt.io>
2023-01-26 00:51:56 +0100

    Add rel attribute to HTML

--- src/ui/modules/About.tsx
+++ src/ui/modules/About.tsx
@@ -18,29 +18,29 @@ export default class About extends React.Component<Props> {
               <div className='type type--xlarge'>UI Color Palette</div>
               <div className='type'>{`Version ${package_json.version.slice(0, 1) + package_json.version.slice(2, 3)}`}</div>
             </div>
-            <p className='type'>Created and maintained by <a href='https://an.gd' target='_blank'>Aurélien Grimaud</a></p>
+            <p className='type'>Created and maintained by <a href='https://an.gd' target='_blank' rel="noreferrer">Aurélien Grimaud</a></p>
         </div>
         <div>
           <div>
             <div className='type type--bold'>Get help</div>
             <div className='about__links'>
-              <button className='button button--tertiary'><a href='https://docs.ui-color-palette.com' target='_blank'>Read the documentation</a></button>
+              <button className='button button--tertiary'><a href='https://docs.ui-color-palette.com' target='_blank' rel="noreferrer">Read the documentation</a></button>
               ﹒
-              <button className='button button--tertiary'><a href='mailto:hello@ui-color-palette.com' target='_blank'>Send an email</a></button>
+              <button className='button button--tertiary'><a href='mailto:hello@ui-color-palette.com' target='_blank' rel="noreferrer">Send an email</a></button>
             </div>
           </div>
           <div>
             <div className='type type--bold'>Be involved</div>
             <div className='about__links'>
-              <button className='button button--tertiary'><a href='https://github.com/inVoltag/figma-ui-color-palette/issues/new' target='_blank'>Open an issue</a></button>
+              <button className='button button--tertiary'><a href='https://github.com/inVoltag/figma-ui-color-palette/issues/new' target='_blank' rel="noreferrer">Open an issue</a></button>
             </div>
           </div>
           <div>
             <div className='type type--bold'>Give support</div>
             <div className='about__links'>
-              <button className='button button--tertiary'><a href='https://twitter.com/a_ng_d' target='_blank'>Follow my activity</a></button>
+              <button className='button button--tertiary'><a href='https://twitter.com/a_ng_d' target='_blank' rel="noreferrer">Follow my activity</a></button>
               ﹒
-              <button className='button button--tertiary'><a href='https://www.buymeacoffee.com/a_ng_d' target='_blank'>Buy me a coffee</a></button>
+              <button className='button button--tertiary'><a href='https://www.buymeacoffee.com/a_ng_d' target='_blank' rel="noreferrer">Buy me a coffee</a></button>
             </div>
           </div>
         </div>
