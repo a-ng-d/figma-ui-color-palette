25c09abcb1f3cd1c3bd50c611dee93ff4445b26d
Aurélien Grimaud <a.grimaud@involt.io>
2023-03-17 21:18:11 +0100

    Insert an unique key to the HelpBar buttons

--- src/ui/modules/HelpBar.tsx
+++ src/ui/modules/HelpBar.tsx
@@ -9,20 +9,22 @@ export default class HelpbBar extends React.Component<Props> {
       <div className="help-bar">
         {this.props.links.map((links, index) =>
           index === this.props.links.length - 1 ? (
-            <button className="button button--tertiary">
-              <a href={links.url} target="_blank" rel="noreferrer">
-                {links.label}
-              </a>
-            </button>
+            <React.Fragment key={links.label}>
+              <button className="button button--tertiary">
+                <a href={links.url} target="_blank" rel="noreferrer">
+                  {links.label}
+                </a>
+              </button>
+            </React.Fragment>
           ) : (
-            <>
+            <React.Fragment key={links.label}>
               <button className="button button--tertiary">
                 <a href={links.url} target="_blank" rel="noreferrer">
                   {links.label}
                 </a>
               </button>
               <span>﹒</span>
-            </>
+            </React.Fragment>
           )
         )}
       </div>
