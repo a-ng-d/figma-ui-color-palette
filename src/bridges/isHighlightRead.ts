6daf8bff987d2dd04e56a59edab86b81d4d32fac
Aurélien Grimaud <a.grimaud@involt.io>
2023-03-20 00:04:26 +0100

    Add a line to debug the Highlight feature

--- src/bridges/isHighlightRead.ts
+++ src/bridges/isHighlightRead.ts
@@ -1,6 +1,7 @@
 import releaseNotes from "../utils/releaseNotes"
 
 const isHighlightRead = async (version: string) => {
+  // figma.clientStorage.deleteAsync(`${version}_isRead`)
   const mostRecentReleaseNoteVersion: string = releaseNotes.filter(note => note['isMostRecent'])[0]['version']
 
   if (mostRecentReleaseNoteVersion != version)
@@ -9,7 +10,7 @@ const isHighlightRead = async (version: string) => {
       data: true
     })
   else {
-  if (await figma.clientStorage.getAsync(`${version}_isRead`) == undefined || await !figma.clientStorage.getAsync(`${version}_isRead`))
+    if (await figma.clientStorage.getAsync(`${version}_isRead`) == undefined || await !figma.clientStorage.getAsync(`${version}_isRead`))
     await figma.ui.postMessage({
       type: 'highlight-status',
       data: false
