4cf21d0fc217e4c1701423ff55e070f445fee83c
Aurélien Grimaud <a.grimaud@involt.io>
2023-03-19 20:11:17 +0100

    Do not render the Highlight if the current version has not one

--- src/bridges/isHighlightRead.ts
+++ src/bridges/isHighlightRead.ts
@@ -1,5 +1,14 @@
+import releaseNotes from "../utils/releaseNotes"
+
 const isHighlightRead = async (version: string) => {
-  // debug figma.clientStorage.deleteAsync(`${version}_isRead`)
+  const mostRecentReleaseNoteVersion: string = releaseNotes.filter(note => note['isMostRecent'])[0]['version']
+
+  if (mostRecentReleaseNoteVersion != version)
+    await figma.ui.postMessage({
+      type: 'highlight-status',
+      data: true
+    })
+  else {
   if (await figma.clientStorage.getAsync(`${version}_isRead`) == undefined || await !figma.clientStorage.getAsync(`${version}_isRead`))
     await figma.ui.postMessage({
       type: 'highlight-status',
@@ -10,6 +19,8 @@ const isHighlightRead = async (version: string) => {
       type: 'highlight-status',
       data: true
     })
+  } 
+  
 }
 
 export default isHighlightRead
