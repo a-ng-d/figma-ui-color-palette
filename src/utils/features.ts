3fbea0126bae13a3cce0742f28774336b2d8b86c
Aurélien Grimaud <a.grimaud@involt.io>
2023-04-22 01:59:07 +0200

    Make a list of the main features

---
+++ src/utils/features.ts
@@ -0,0 +1,33 @@
+interface Features {
+  name: string
+  description: string
+  isPro: boolean
+  isActive: boolean
+}
+
+export const features: Array<Features> = [
+  {
+    name: 'onboarding',
+    description: 'Onboarding service when the selection is empty',
+    isActive: true,
+    isPro: false
+  },
+  {
+    name: 'highlight',
+    description: 'Release note that highlights the key feature',
+    isActive: true,
+    isPro: false
+  },
+  {
+    name: 'create palette',
+    description: 'Palette creation service when several colors are selected',
+    isActive: true,
+    isPro: false
+  },
+  {
+    name: 'edit palette',
+    description: 'Palette configuration service when the palette is selected',
+    isActive: true,
+    isPro: false
+  }
+]
