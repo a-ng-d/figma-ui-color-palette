66fc5f7cfd8f4b120f12008ef3d533f5bd700a04
Aurélien Grimaud <a.grimaud@involt.io>
2023-05-06 17:58:43 +0200

    Centralize types in one single source of truth

---
+++ src/utils/types.ts
@@ -0,0 +1,55 @@
+export interface Preset {
+  name: string
+  scale: Array<number>
+  min: number
+  max: number
+}
+
+export interface Scale {
+  [key: string]: string
+}
+
+export interface Palette {
+  name: string
+  scale: Scale
+  min: number
+  max: number
+  properties: boolean
+  preset: any
+}
+
+export interface Features {
+  name: string
+  description: string
+  isActive: boolean
+  isPro: boolean
+  type: string
+  service: Array<string>
+}
+
+export interface ReleaseNote {
+  version: string
+  isMostRecent: boolean
+  title: string
+  image: string
+  content: string
+  learnMore: string
+}
+
+export interface UIColors {
+  name: string
+  rgb: {
+    r: number
+    g: number
+    b: number
+  }
+  id: string | undefined
+  oklch: boolean
+  hueShifting: number
+}
+
+export interface Rgb {
+  r: number
+  g: number
+  b: number
+}
