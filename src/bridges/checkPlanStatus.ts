fb17d0d52616e37a5fabc15dd74330b3ccfebc95
Aurélien Grimaud <a.grimaud@involt.io>
2023-05-26 20:53:51 +0200

    Move the plan status

--- src/bridges/checkPlanStatus.ts
+++ src/bridges/checkPlanStatus.ts
@@ -1,4 +1,7 @@
 const checkPlanStatus = async () => {
+  await figma.payments.setPaymentStatusInDevelopment({
+    type: 'UNPAID'
+  })
   await figma.ui.postMessage({
     type: 'plan-status',
     data: figma.payments.status.type,
--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,8 @@ import exportJson from './bridges/exportJson'
 import exportCss from './bridges/exportCss'
 import exportCsv from './bridges/exportCsv'
 import updateSettings from './bridges/updateSettings'
+import getProPlan from './bridges/getProPlan'
 import package_json from './../package.json'
-
-figma.payments.setPaymentStatusInDevelopment({
-  type: 'UNPAID'
-})
 
 figma.showUI(__html__, {
   width: 640,
@@ -33,9 +30,9 @@ figma.on('run', () => processSelection())
 figma.on('selectionchange', () => processSelection())
 
 figma.on('run', () => isHighlightRead(package_json.version))
-figma.on('run', () => checkPlanStatus())
+figma.on('run', async () => await checkPlanStatus())
 
-figma.ui.onmessage = (msg) => {
+figma.ui.onmessage = async (msg) => {
   let palette: ReadonlyArray<SceneNode>
   const i = 0
 
@@ -76,5 +73,9 @@ figma.ui.onmessage = (msg) => {
 
     case 'update-settings':
       updateSettings(msg, palette)
+      break
+    
+    case 'get-pro-plan':
+      await getProPlan()
   }
 }
