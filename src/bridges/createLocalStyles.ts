682711e95ad69608cda7e07ed0d8c8cd0c87a83d
Aurélien Grimaud <a.grimaud@involt.io>
2023-06-04 01:48:58 +0200

    Generate color styles from the plugin data

--- src/bridges/createLocalStyles.ts
+++ src/bridges/createLocalStyles.ts
@@ -1,37 +1,56 @@
 import Style from './../canvas/Style'
+import Colors from '../canvas/Colors'
 
 const createLocalStyles = (palette, i: number) => {
   palette = figma.currentPage.selection[0] as FrameNode
+  console.log(palette.getPluginData('colors'))
 
   if (palette.children.length == 1) {
-    const localStyles = figma.getLocalPaintStyles()
+    const localStyles: Array<PaintStyle> = figma.getLocalPaintStyles()
+    let source: PaintStyle, shade
     i = 0
-    let source
 
-    palette.children[0].children.forEach((row) => {
-      if (row.name != '_header' && row.name != '_title')
-        row.children.forEach((sample, index) => {
-          if (
-            localStyles.filter(
-              (e) =>
-                e.name ===
-                `${row.name}/${sample.name.replace(row.name + '-', '')}`
-            ).length == 0 &&
-            localStyles.filter((e) => e.name === `${row.name}/source`).length ==
-              0
-          ) {
-            const style = new Style(
-              `${row.name}/${
-                index === 0 ? 'source' : sample.name.replace(row.name + '-', '')
-              }`,
-              sample.fills[0].color
-            ).makeNode()
-            index === 0
-              ? (source = style)
-              : figma.moveLocalPaintStyleAfter(style, source)
-            i++
+    JSON.parse(palette.getPluginData('colors')).forEach(color => {
+      if (localStyles.filter((e) => e.name === `${color.name}/source`).length == 0) {
+        source = new Style(
+          `${color.name}/source`,
+          {
+            r: color.rgb.r,
+            g: color.rgb.g,
+            b: color.rgb.b
           }
-        })
+        ).makeNode()
+        i++
+      }
+      Object.entries(JSON.parse(palette.getPluginData('scale'))).forEach((lightness: any) => {
+        if (
+          localStyles.filter(
+            (e) =>
+              e.name ===
+              `${color.name}/${lightness[0].replace('lightness-', '')}`
+          ).length == 0
+        ) {
+          shade = new Colors().getShadeColorFromLch(
+            [
+              color.rgb.r * 255,
+              color.rgb.g * 255,
+              color.rgb.b * 255,
+            ],
+            lightness[1],
+            color.hueShifting,
+            palette.getPluginData('algorithmVersion')
+          )
+          const style = new Style(
+            `${color.name}/${lightness[0].replace('lightness-', '')}`,
+            {
+              r: shade._rgb[0] / 255,
+              g: shade._rgb[1] / 255,
+              b: shade._rgb[2] / 255
+            }
+          ).makeNode()
+          i++
+        }
+      })
     })
 
     if (i > 1) figma.notify(`${i} local color styles have been created`)
