760fc94ab8e6231e33d0c3269758b37a18a87a2a
Aurélien Grimaud <a.grimaud@involt.io>
2023-06-05 01:19:42 +0200

    Simplify the local styles creation from the data

--- src/bridges/createLocalStyles.ts
+++ src/bridges/createLocalStyles.ts
@@ -1,55 +1,27 @@
-import type { ColorConfiguration } from '../utils/types'
+import type { PaletteDataItem } from '../utils/types'
 import Style from './../canvas/Style'
-import Colors from '../canvas/Colors'
 
 const createLocalStyles = (palette, i: number) => {
   palette = figma.currentPage.selection[0] as FrameNode
   const localStyles: Array<PaintStyle> = figma.getLocalPaintStyles()
-  let source: PaintStyle, shade: { _rgb: Array<number> }
 
   if (palette.children.length == 1) {
     i = 0
 
-    JSON.parse(palette.getPluginData('colors')).forEach((color: ColorConfiguration) => {
-      if (localStyles.filter((e) => e.name === `${color.name}/source`).length == 0) {
-        source = new Style(
-          `${color.name}/source`,
-          {
-            r: color.rgb.r,
-            g: color.rgb.g,
-            b: color.rgb.b
-          }
-        ).makeNode()
-        i++
-      }
-      Object.entries(JSON.parse(palette.getPluginData('scale'))).forEach((lightness: any) => {
-        if (
-          localStyles.filter(
-            (e) =>
-              e.name ===
-              `${color.name}/${lightness[0].replace('lightness-', '')}`
-          ).length == 0
-        ) {
-          shade = new Colors().getShadeColorFromLch(
-            [
-              color.rgb.r * 255,
-              color.rgb.g * 255,
-              color.rgb.b * 255,
-            ],
-            lightness[1],
-            color.hueShifting,
-            palette.getPluginData('algorithmVersion')
-          )
-          const style = new Style(
-            `${color.name}/${lightness[0].replace('lightness-', '')}`,
-            {
-              r: shade._rgb[0] / 255,
-              g: shade._rgb[1] / 255,
-              b: shade._rgb[2] / 255
-            }
-          ).makeNode()
+    JSON.parse(palette.getPluginData('data')).forEach((color: PaletteDataItem) => {
+      color.shades.forEach(shade => {
+        if (localStyles.filter((e) => e.name === `${color.name}/${shade.name}`).length == 0) {
+          const style: PaintStyle =
+            new Style(
+              `${color.name}/${shade.name}`,
+              {
+                r: shade.gl[0],
+                g: shade.gl[1],
+                b: shade.gl[2]
+              }
+            ).makeNode()
           i++
-        }
+        }      
       })
     })
 
