283abbdb5d7558946840af7eaf78a6e0fd9e11e8
Aurélien Grimaud <a.grimaud@involt.io>
2023-06-05 01:29:51 +0200

    Update local styles from the palette data

--- src/bridges/updateLocalStyles.ts
+++ src/bridges/updateLocalStyles.ts
@@ -1,25 +1,34 @@
-const updateLocalStyles = (palette, i) => {
+import chroma from 'chroma-js'
+import type { PaletteDataItem } from '../utils/types'
+
+const updateLocalStyles = (palette, i: number) => {
   palette = figma.currentPage.selection[0]
+  const localStyles: Array<PaintStyle> = figma.getLocalPaintStyles()
+  let target: PaintStyle
 
   if (palette.children.length == 1) {
-    const localStyles = figma.getLocalPaintStyles()
     i = 0
 
-    palette.children[0].children.forEach((row) => {
-      row.children.forEach((sample) => {
-        localStyles.forEach((localStyle) => {
-          if (`${row.name}/${sample.name}` === localStyle.name) {
-            if (
-              JSON.stringify(localStyle.paints[0]['color']) !=
-              JSON.stringify(sample.fills[0]['color'])
-            ) {
-              localStyle.paints = sample.fills
-              i++
+    JSON.parse(palette.getPluginData('data')).forEach((color: PaletteDataItem) => {
+      color.shades.forEach(shade => {
+        target = localStyles.filter(localStyle => localStyle.name === `${color.name}/${shade.name}`)[0]
+
+        if (shade.hex != chroma([target.paints[0]['color'].r * 255, target.paints[0]['color'].g * 255, target.paints[0]['color'].b * 255]).hex()) {
+          target.paints = [
+            {
+              type: 'SOLID',
+              color: {
+                r: shade.gl[0],
+                g: shade.gl[1],
+                b: shade.gl[2]
+              }
             }
-          }
-        })
+          ]
+          i++
+        }
       })
     })
+    
     if (i > 1) figma.notify(`${i} local color styles have been updated`)
     else if (i == 1) figma.notify(`${i} local color style has been updated`)
     else figma.notify(`No color has been updated because the palette has not been edited`)
