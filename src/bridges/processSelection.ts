95cef3243489694b3fe6b47177e21d1b023f4656
Aurélien Grimaud <a.grimaud@involt.io>
2023-06-15 16:48:29 +0200

    Isolate palette migration actions

--- src/bridges/processSelection.ts
+++ src/bridges/processSelection.ts
@@ -1,6 +1,4 @@
-import setData from './../utils/setData'
-import { presets } from './../utils/palettePackage'
-import Colors from '../canvas/Colors'
+import setPaletteMigration from '../utils/setPaletteMigration'
 
 export let currentSelection: ReadonlyArray<SceneNode>
 export let previousSelection: ReadonlyArray<SceneNode>
@@ -18,73 +16,7 @@ const processSelection = () => {
     const palette: BaseNode = selection[0]
 
     // Migration
-    if (palette.getPluginData('min') != '' || palette.getPluginData('max')) {
-      palette.setPluginData('min', '')
-      palette.setPluginData('max', '')
-    }
-
-    if (palette.getPluginData('preset') === '')
-      palette.setPluginData('preset', JSON.stringify(presets.material))
-
-    if (palette.getPluginData('algorithmVersion') === '')
-      palette.setPluginData('algorithmVersion', 'v1')
-
-    if (!palette.getPluginData('colors').includes('oklch'))
-      palette.setPluginData(
-        'colors',
-        setData(palette.getPluginData('colors'), 'oklch', false)
-      )
-
-    if (!palette.getPluginData('colors').includes('hueShifting'))
-      palette.setPluginData(
-        'colors',
-        setData(palette.getPluginData('colors'), 'hueShifting', 0)
-      )
-
-    if (
-      palette.getPluginData('captions') == 'hasCaptions' ||
-      palette.getPluginData('properties') == 'hasProperties'
-    ) {
-      palette.setPluginData('captions', '')
-      palette.setPluginData('properties', '')
-      palette.setPluginData('view', 'PALETTE_WITH_PROPERTIES')
-    } else if (
-      palette.getPluginData('captions') == 'hasNotCaptions' ||
-      palette.getPluginData('properties') == 'hasNotProperties'
-    ) {
-      palette.setPluginData('captions', '')
-      palette.setPluginData('properties', '')
-      palette.setPluginData('view', 'PALETTE')
-    }
-
-    if (palette.getPluginData('textColorsTheme') === '') {
-      palette.setPluginData(
-        'textColorsTheme',
-        JSON.stringify({
-          lightColor: '#FFFFFF',
-          darkColor: '#000000',
-        })
-      )
-    }
-
-    if (palette.getPluginData('view') === '')
-      palette.setPluginData('view', 'PALETTE')
-
-    if (palette.getPluginData('data') === '')
-      new Colors(
-        {
-          paletteName: palette.getPluginData('name'),
-          preset: JSON.parse(palette.getPluginData('preset')),
-          scale: JSON.parse(palette.getPluginData('scale')),
-          colors: JSON.parse(palette.getPluginData('colors')),
-          view: 'SHEET',
-          textColorsTheme: JSON.parse(palette.getPluginData('textColorsTheme')),
-          algorithmVersion: palette.getPluginData('algorithmVersion'),
-        },
-        selection[0] as FrameNode
-      ).makePaletteData()
-    
-    // console.log(palette.getPluginDataKeys())
+    setPaletteMigration(palette)
     
     // to UI
     figma.ui.postMessage({
