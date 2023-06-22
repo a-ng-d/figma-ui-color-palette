83e9f1bdf55a41d246a8f27190ec0892a455d67c
Aurélien Grimaud <a.grimaud@involt.io>
2023-06-23 00:28:44 +0200

    Remove the last broken line

--- src/bridges/exportSwift.ts
+++ src/bridges/exportSwift.ts
@@ -15,9 +15,11 @@ const exportSwift = (palette) => {
             `static let ${color.name.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())}${shade.name === 'source' ? 'Source' : shade.name} = Color(red: ${shade.gl[0].toFixed(3)}, green: ${shade.gl[1].toFixed(3)}, blue: ${shade.gl[2].toFixed(3)})`
           )
         })
+        UIColors.unshift('')
         UIColors.reverse().forEach((UIColor) => swift.push(UIColor))
       }
     )
+    swift.pop()
     figma.ui.postMessage({
       type: 'EXPORT_PALETTE_SWIFT',
       data: swift,
