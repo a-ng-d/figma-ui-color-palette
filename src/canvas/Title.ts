f6059fb8b6c6de8a0779773a3dad6d93c757721e
Aurélien Grimaud <a.grimaud@involt.io>
2023-06-28 17:11:26 +0200

    Refactor the palette title part

--- src/canvas/Title.ts
+++ src/canvas/Title.ts
@@ -1,9 +1,11 @@
 import type { PaletteNode } from '../utils/types'
-import Sample from './Sample'
+import Tag from './Tag'
 
 export default class Title {
   text: string
   parent: PaletteNode
+  nodeName: FrameNode
+  nodeProps: FrameNode
   node: FrameNode
 
   constructor(text: string, parent: PaletteNode) {
@@ -11,11 +13,74 @@ export default class Title {
     this.parent = parent
   }
 
+  makeNodeName() {
+    // base
+    this.nodeName = figma.createFrame()
+    this.nodeName.name = '_palette-name'
+    this.nodeName.fills = []
+
+    // layout
+    this.nodeName.layoutMode = 'VERTICAL'
+    this.nodeName.primaryAxisSizingMode = 'AUTO'
+    this.nodeName.counterAxisSizingMode = 'FIXED'
+    this.nodeName.layoutGrow = 1
+
+    // insert
+    this.nodeName.appendChild(
+      new Tag(
+        '_name',
+        this.parent.paletteName,
+        20
+      ).makeNodeTag()
+    )
+
+    return this.nodeName
+  }
+
+  makeNodeProps() {
+    // base
+    this.nodeProps = figma.createFrame()
+    this.nodeProps.name = '_palette-props'
+    this.nodeProps.fills = []
+
+    // layout
+    this.nodeProps.layoutMode = 'VERTICAL'
+    this.nodeProps.primaryAxisSizingMode = 'AUTO'
+    this.nodeProps.counterAxisSizingMode = 'FIXED'
+    this.nodeProps.counterAxisAlignItems = 'MAX'
+    this.nodeProps.itemSpacing = 4
+    this.nodeProps.layoutGrow = 1
+
+    // insert
+    this.nodeProps.appendChild(
+      new Tag(
+        '_preset',
+        `Preset: ${this.parent.preset.name}`,
+        12
+      ).makeNodeTag()
+    )
+    this.nodeProps.appendChild(
+      new Tag(
+        '_view',
+        `Layout: ${this.parent.view.includes('PALETTE') ? 'Palette' : 'Sheet'}`,
+        12
+      ).makeNodeTag()
+    )
+    this.nodeProps.appendChild(
+      new Tag(
+        '_color-space',
+        `Color space: ${this.parent.colorSpace}`,
+        12
+      ).makeNodeTag()
+    )
+
+    return this.nodeProps
+  }
+
   makeNode() {
     // base
     this.node = figma.createFrame()
     this.node.name = '_title'
-    this.node.resize(100, 48)
     this.node.fills = []
 
     // layout
@@ -26,15 +91,10 @@ export default class Title {
 
     // insert
     this.node.appendChild(
-      new Sample(
-        this.text,
-        null,
-        null,
-        [255, 255, 255],
-        this.parent.colorSpace,
-        this.parent.view,
-        this.parent.textColorsTheme
-      ).makeNodeName('RELATIVE', 100, 48)
+      this.makeNodeName()
+    )
+    this.node.appendChild(
+      this.makeNodeProps()
     )
 
     return this.node
