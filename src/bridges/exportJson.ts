88fd3feb099290d2ba0b38faf1dfc8c1fcaf2e6f
Aurélien Grimaud <a.grimaud@involt.io>
2023-06-28 19:23:20 +0200

    Add more color models within the JSON export

--- src/bridges/exportJson.ts
+++ src/bridges/exportJson.ts
@@ -15,15 +15,35 @@ const exportJson = (palette) => {
               g: Math.floor(shade.rgb[1]),
               b: Math.floor(shade.rgb[2]),
             },
+            gl: {
+              r: parseFloat(shade.gl[0].toFixed(3)),
+              g: parseFloat(shade.gl[1].toFixed(3)),
+              b: parseFloat(shade.gl[2].toFixed(3)),
+            },
             lch: {
               l: Math.floor(shade.lch[0]),
               c: Math.floor(shade.lch[1]),
               h: Math.floor(shade.lch[2]),
             },
-            gl: {
-              r: parseFloat(shade.gl[0].toFixed(3)),
-              g: parseFloat(shade.gl[1].toFixed(3)),
-              b: parseFloat(shade.gl[2].toFixed(3)),
+            oklch: {
+              l: parseFloat(shade.oklch[0].toFixed(3)),
+              c: parseFloat(shade.oklch[1].toFixed(3)),
+              h: Math.floor(shade.oklch[2]),
+            },
+            lab: {
+              l: Math.floor(shade.lab[0]),
+              a: Math.floor(shade.lab[1]),
+              b: Math.floor(shade.lab[2]),
+            },
+            oklab: {
+              l: parseFloat(shade.oklab[0].toFixed(3)),
+              a: parseFloat(shade.oklab[1].toFixed(3)),
+              b: parseFloat(shade.oklab[2].toFixed(3)),
+            },
+            hsl: {
+              h: Math.floor(shade.hsl[0]),
+              s: Math.floor(shade.hsl[1] * 100),
+              l: Math.floor(shade.hsl[2] * 100),
             },
             hex: shade.hex,
             type: 'color',
