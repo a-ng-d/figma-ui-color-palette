3e07b4199f516a98984c96e805a885b872eb5650
Aurélien Grimaud <a.grimaud@involt.io>
2023-07-14 00:01:53 +0200

    Create as many header cells as lightness stop

--- src/canvas/Header.ts
+++ src/canvas/Header.ts
@@ -1,14 +1,16 @@
-import type { PaletteNode } from '../utils/types'
+import type { PaletteNode, ScaleConfiguration } from '../utils/types'
 import Sample from './Sample'
 import { locals, lang } from '../content/locals'
 
 export default class Header {
   parent: PaletteNode
+  currentScale: ScaleConfiguration
   sampleSize: number
   node: FrameNode
 
   constructor(parent: PaletteNode, size: number) {
     this.parent = parent
+    this.currentScale = this.parent.themes.find(theme => theme.isEnabled).scale
     this.sampleSize = size
   }
 
@@ -37,13 +39,13 @@ export default class Header {
       ).makeNodeName('ABSOLUTE', this.sampleSize, 48)
     )
     if (this.parent.view.includes('PALETTE'))
-      Object.values(this.parent.scale)
+      Object.values(this.currentScale)
         .reverse()
         .forEach((lightness) => {
           this.node.appendChild(
             new Sample(
-              Object.keys(this.parent.scale)
-                .find((key) => this.parent.scale[key] === lightness)
+              Object.keys(this.currentScale)
+                .find((key) => this.currentScale[key] === lightness)
                 .substr(10),
               null,
               null,
