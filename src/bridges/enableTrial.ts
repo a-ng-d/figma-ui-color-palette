c58a92f6f7a68ab14450049214c050f7632430e1
Aurélien Grimaud <a.grimaud@involt.io>
2023-10-12 00:29:05 +0200

    feature: Push the trial data on the Notion analytics tool

--- src/bridges/enableTrial.ts
+++ src/bridges/enableTrial.ts
@@ -1,10 +1,70 @@
+import { NOTION_API_KEY, NOTION_TRIAL_TABLE_ID } from './../../env.s'
+
 const enableTrial = async () => {
+  const date = new Date()
+
   await figma.clientStorage
-    .setAsync('trial_start_date', new Date().getTime())
+    .setAsync('trial_start_date', date.getTime())
     .then(() => {
       figma.ui.postMessage({
         type: 'ENABLE_TRIAL',
       })
+    })
+    .then(() => {
+      fetch(
+        'https://corsproxy.io/?' +
+          encodeURIComponent('https://api.notion.com/v1/pages'),
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${NOTION_API_KEY}`,
+            Accept: 'application/json',
+            'Access-Control-Allow-Origin': 'https://www.figma.com/**/*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Notion-Version': '2022-06-28',
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            parent: {
+              database_id: NOTION_TRIAL_TABLE_ID,
+            },
+            properties: {
+              Utilisateur: {
+                title: [
+                  {
+                    type: 'text',
+                    text: {
+                      content: figma.currentUser?.name ?? 'NC',
+                      link: null,
+                    },
+                    annotations: {
+                      bold: false,
+                      italic: false,
+                      strikethrough: false,
+                      underline: false,
+                      code: false,
+                      color: 'default',
+                    },
+                    plain_text: figma.currentUser?.name ?? 'NC',
+                    href: null,
+                  },
+                ],
+              },
+              "Période de l'essai": {
+                date: {
+                  start: date.toISOString(),
+                  end: new Date(date.getTime() + 259200000).toISOString(),
+                },
+              },
+            },
+          }),
+          cache: 'no-cache',
+          credentials: 'omit',
+        }
+      )
+        .then((response) => response.json())
+        .then((response) => console.log(response))
+        .catch((err) => console.error(err))
     })
 }
 
