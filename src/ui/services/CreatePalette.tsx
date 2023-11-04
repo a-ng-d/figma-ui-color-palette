6463b934ad88956470822dc06432af831ed95598
Aurélien Grimaud <a.grimaud@involt.io>
2023-11-04 03:20:24 +0100

    feat: display the current selection in the controller

--- src/ui/services/CreatePalette.tsx
+++ src/ui/services/CreatePalette.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
+import chroma from 'chroma-js'
 import type {
   Language,
   PresetConfiguration,
+  SourceColorConfiguration,
   TextColorsThemeHexModel,
 } from '../../utils/types'
+import Message from '../components/Message'
 import Bar from '../components/Bar'
 import Tabs from '../components/Tabs'
 import Scale from '../modules/Scale'
@@ -11,8 +14,10 @@ import Settings from '../modules/Settings'
 import { palette } from '../../utils/palettePackage'
 import features from '../../utils/config'
 import { locals } from '../../content/locals'
+import Actions from '../modules/Actions'
 
 interface Props {
+  sourceColors: Array<SourceColorConfiguration> | []
   name: string
   description: string
   preset: PresetConfiguration
@@ -55,8 +60,14 @@ export default class CreatePalette extends React.Component<Props, any> {
 
   // Direct actions
   onCreatePalette = () =>
-    parent.postMessage(
-      { pluginMessage: { type: 'CREATE_PALETTE', data: palette } },
+    parent.postMessage({
+      pluginMessage: {
+        type: 'CREATE_PALETTE',
+        data: {
+          sourceColors: this.props.sourceColors,
+          palette: palette,
+        }
+      }},
       '*'
     )
 
@@ -89,9 +100,91 @@ export default class CreatePalette extends React.Component<Props, any> {
     let controls
 
     switch (this.state['context']) {
+      case 'SOURCE': {
+        controls = (
+          <>
+            <div className="list-controller controls__control controls__control--horizontal">
+              <div className="controls__control__part">
+                <div className="section-controls">
+                  <div className="section-controls__left-part">
+                    <div className="section-title">
+                      {locals[this.props.lang].source.canvas.title}
+                    </div>
+                    <div className="type">{`(${this.props.sourceColors.filter(sourceColor => sourceColor.source === 'CANVAS').length})`}</div>
+                  </div>
+                  <div className="section-controls__right-part">
+                  </div>
+                </div>
+                {
+                  this.props.sourceColors.filter(sourceColor => sourceColor.source === 'CANVAS').length > 0
+                  ? (
+                    <ul className="list">
+                      {this.props.sourceColors.map((sourceColor, index) => {
+                        return (
+                          <li
+                            key={index}
+                            className="list__item"
+                          >
+                            <div className="source-colors">
+                              <div
+                                className="source-colors__thumbnail"
+                                style={{
+                                  backgroundColor: `rgb(${sourceColor.rgb.r * 255}, ${sourceColor.rgb.g * 255}, ${sourceColor.rgb.b * 255})`
+                                }}
+                              ></div>
+                              <div className="source-colors__name">
+                                <div className="type">
+                                  {sourceColor.name}
+                                </div>
+                                <div className="type type--secondary">
+                                  {`(${
+                                    chroma(sourceColor.rgb.r * 255, sourceColor.rgb.g * 255, sourceColor.rgb.b * 255)
+                                      .hex()
+                                      .toUpperCase()
+                                  })`}
+                                </div>
+                              </div>
+                            </div>
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  )
+                  : (
+                    <Message
+                      icon="list-tile"
+                      messages={[locals[this.props.lang].onboarding.selectColor]}
+                    />
+                  )
+                }   
+              </div>
+              <div className="controls__control__part">
+                <div className="section-controls">
+                  <div className="section-controls__left-part">
+                    <div className="section-title">
+                      {locals[this.props.lang].source.coolors.title}
+                    </div>
+                  </div>
+                  <div className="section-controls__right-part">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Actions
+              context="CREATE"
+              sourceColors={this.props.sourceColors}
+              planStatus={this.props.planStatus}
+              lang={this.props.lang}
+              onCreatePalette={this.props.sourceColors.length > 0 ? this.onCreatePalette : () => null}
+            />
+          </>
+        )
+        break
+      }
       case 'SCALE': {
         controls = (
           <Scale
+            sourceColors={this.props.sourceColors}
             hasPreset={true}
             preset={this.props.preset}
             planStatus={this.props.planStatus}
@@ -109,6 +202,7 @@ export default class CreatePalette extends React.Component<Props, any> {
         controls = (
           <Settings
             context="CREATE"
+            sourceColors={this.props.sourceColors}
             name={this.props.name}
             description={this.props.description}
             colorSpace={this.props.colorSpace}
