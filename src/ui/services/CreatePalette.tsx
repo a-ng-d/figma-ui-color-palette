694963a4e88422c6f01e3b3043fca232caeef8c6
Aurélien Grimaud <a.grimaud@involt.io>
2023-11-05 16:57:00 +0100

    refactor: isolate Source context

--- src/ui/services/CreatePalette.tsx
+++ src/ui/services/CreatePalette.tsx
@@ -6,7 +6,6 @@ import type {
   SourceColorConfiguration,
   TextColorsThemeHexModel,
 } from '../../utils/types'
-import Message from '../components/Message'
 import Bar from '../components/Bar'
 import Tabs from '../components/Tabs'
 import Scale from '../modules/Scale'
@@ -14,10 +13,8 @@ import Settings from '../modules/Settings'
 import { palette } from '../../utils/palettePackage'
 import features from '../../utils/config'
 import { locals } from '../../content/locals'
-import Actions from '../modules/Actions'
 import doLightnessScale from '../../utils/doLightnessScale'
-import Input from '../components/Input'
-import FormItem from '../components/FormItem'
+import Source from '../modules/Source'
 
 interface Props {
   sourceColors: Array<SourceColorConfiguration> | []
@@ -62,34 +59,6 @@ export default class CreatePalette extends React.Component<Props, any> {
     this.setState({
       context: (e.target as HTMLElement).dataset.feature,
     })
-  
-  importCoolorsHandler = (e: React.SyntheticEvent) => {
-    const url = (e.target as HTMLInputElement).value,
-      domain = url.split('/')[3],
-      hexs = url.split('/').at(-1)
-
-    if (hexs != undefined)
-      if (/[A-Za-z0-9]+-[A-Za-z0-9]+/i.test(hexs)) {
-        this.props.onImportCoolors(
-          hexs.split('-').map(hex => {
-            const gl = chroma(hex).gl()
-            return {
-              name: hex,
-              rgb: {
-                r: gl[0],
-                g: gl[1],
-                b: gl[2]
-              },
-              source: 'COOLORS'
-            }
-          }
-        ))
-        this.setState({
-          coolorsUrl: ''
-        })
-      }
-
-  }
 
   // Direct actions
   onCreatePalette = () =>
@@ -140,132 +109,13 @@ export default class CreatePalette extends React.Component<Props, any> {
     switch (this.state['context']) {
       case 'SOURCE': {
         controls = (
-          <>
-            <div className="controls__control controls__control--horizontal">
-              <div className="control__block control__block--list">
-                <div className="section-controls">
-                  <div className="section-controls__left-part">
-                    <div className="section-title">
-                      {locals[this.props.lang].source.canvas.title}
-                    </div>
-                    <div className="type">{`(${this.props.sourceColors.filter(sourceColor => sourceColor.source === 'CANVAS').length})`}</div>
-                  </div>
-                  <div className="section-controls__right-part">
-                  </div>
-                </div>
-                {
-                  this.props.sourceColors.filter(sourceColor => sourceColor.source === 'CANVAS').length > 0
-                  ? (
-                    <ul className="list">
-                      {this.props.sourceColors.filter(sourceColor => sourceColor.source === 'CANVAS').map((sourceColor, index) => {
-                        return (
-                          <li
-                            key={index}
-                            className="list__item"
-                          >
-                            <div className="source-colors">
-                              <div
-                                className="source-colors__thumbnail"
-                                style={{
-                                  backgroundColor: `rgb(${sourceColor.rgb.r * 255}, ${sourceColor.rgb.g * 255}, ${sourceColor.rgb.b * 255})`
-                                }}
-                              ></div>
-                              <div className="source-colors__name">
-                                <div className="type">
-                                  {sourceColor.name}
-                                </div>
-                                <div className="type type--secondary">
-                                  {`(${
-                                    chroma(sourceColor.rgb.r * 255, sourceColor.rgb.g * 255, sourceColor.rgb.b * 255)
-                                      .hex()
-                                      .toUpperCase()
-                                  })`}
-                                </div>
-                              </div>
-                            </div>
-                          </li>
-                        )
-                      })}
-                    </ul>
-                  )
-                  : (
-                    <Message
-                      icon="list-tile"
-                      messages={[locals[this.props.lang].onboarding.selectColor]}
-                    />
-                  )
-                }   
-              </div>
-              <div className="control__block control__block--list">
-                <div className="section-controls">
-                  <div className="section-controls__left-part">
-                    <div className="section-title">
-                      {locals[this.props.lang].source.coolors.title}
-                    </div>
-                    <div className="type">{`(${this.props.sourceColors.filter(sourceColor => sourceColor.source === 'COOLORS').length})`}</div>
-                  </div>
-                  <div className="section-controls__right-part">
-                  </div>
-                </div>
-                <div>
-                  <FormItem
-                    id="coolors-palette-urn"
-                    label="Paste your palette URL"
-                  >
-                    <Input
-                      type="TEXT"
-                      placeholder="https://coolors.co/…"
-                      value={this.state['coolorsUrl']}
-                      onChange={(e: React.SyntheticEvent) => this.setState({
-                        coolorsUrl: (e.target as HTMLInputElement).value
-                      })}
-                      onFocus={() => null}
-                      onBlur={this.importCoolorsHandler}
-                      onConfirm={() => null}
-                    />
-                  </FormItem>
-                </div>
-                <ul className="list">
-                  {this.props.sourceColors.filter(sourceColor => sourceColor.source === 'COOLORS').map((sourceColor, index) => {
-                    return (
-                      <li
-                        key={index}
-                        className="list__item"
-                      >
-                        <div className="source-colors">
-                          <div
-                            className="source-colors__thumbnail"
-                            style={{
-                              backgroundColor: `rgb(${sourceColor.rgb.r * 255}, ${sourceColor.rgb.g * 255}, ${sourceColor.rgb.b * 255})`
-                            }}
-                          ></div>
-                          <div className="source-colors__name">
-                            <div className="type">
-                              {sourceColor.name}
-                            </div>
-                            <div className="type type--secondary">
-                              {`(${
-                                chroma(sourceColor.rgb.r * 255, sourceColor.rgb.g * 255, sourceColor.rgb.b * 255)
-                                  .hex()
-                                  .toUpperCase()
-                              })`}
-                            </div>
-                          </div>
-                        </div>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-            </div>
-            <Actions
-              context="CREATE"
-              sourceColors={this.props.sourceColors}
-              planStatus={this.props.planStatus}
-              lang={this.props.lang}
-              onCreatePalette={this.props.sourceColors.length > 0 ? this.onCreatePalette : () => null}
-            />
-          </>
+          <Source
+            sourceColors={this.props.sourceColors}
+            planStatus={this.props.planStatus}
+            lang={this.props.lang}
+            onImportCoolors={this.props.onImportCoolors}
+            onCreatePalette={this.onCreatePalette}
+          />
         )
         break
       }
