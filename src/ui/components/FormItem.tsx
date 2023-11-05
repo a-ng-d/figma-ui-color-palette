db135c11376769fc0186122475d1b4c2b4233f47
Aurélien Grimaud <a.grimaud@involt.io>
2023-11-05 21:55:42 +0100

    feat: display helper on forms to guide user

--- src/ui/components/FormItem.tsx
+++ src/ui/components/FormItem.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react'
+import Message from './Message'
 
 interface Props {
   id: string
   label: string
+  helper?: {
+    type: 'INFO' | 'ERROR'
+    message: string
+  }
   shouldFill?: boolean
   isBlocked?: boolean
   children: React.ReactNode
@@ -16,19 +21,30 @@ export default class FormItem extends React.Component<Props> {
 
   render() {
     return (
-      <div
-        className={`form-item form-item${
-          this.props.shouldFill ? '--fill' : ''
-        }${this.props.isBlocked ? ' form-item--blocked' : ''}`}
-      >
-        <label
-          className="type"
-          htmlFor={this.props.id}
+      <>
+        <div
+          className={`form-item form-item${
+            this.props.shouldFill ? '--fill' : ''
+          }${this.props.isBlocked ? ' form-item--blocked' : ''}`}
         >
-          {this.props.label}
-        </label>
-        {this.props.children}
-      </div>
+          <label
+            className="type"
+            htmlFor={this.props.id}
+          >
+            {this.props.label}
+          </label>
+          {this.props.children}        
+        </div>
+        {this.props.helper != undefined ? (
+          <div className="form-item__helper">
+            <Message
+              icon={this.props.helper.type === 'INFO' ? 'key' : 'warning'}
+              messages={[this.props.helper.message]}
+            />
+          </div>
+          ) : null
+        }
+      </>
     )
   }
 }
