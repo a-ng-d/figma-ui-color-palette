d80075f1678295c756c5deb08098297bf0b958e5
Aurélien Grimaud <a.grimaud@involt.io>
2023-11-07 02:32:50 +0100

    feat: add two more parameters

--- src/index.ts
+++ src/index.ts
@@ -47,13 +47,37 @@ figma.parameters.on(
           .length != 0
       )
     switch (key) {
-      case 'preset':
-        const scale = presets.map(preset => preset.name)
+      case 'preset': {
         if (viableSelection.length > 0)
-          result.setSuggestions(scale.filter(s => s.includes(query)))
+          result.setSuggestions(presets.map(preset => preset.name).filter(s => s.includes(query)))
         else
           result.setError('Select a layer that is filled with at least one solid color')
+        result.setLoadingMessage('Palette presets will be displayed here')
         break
+      }
+
+      case 'space': {
+        result.setSuggestions([
+          'LCH',
+          'OKLCH',
+          'LAB',
+          'OKLAB',
+          'HSL',
+          'HSLuv'
+        ])
+        result.setLoadingMessage('Color spaces will be displayed here')
+        break
+      }
+
+      case 'view': {
+        result.setSuggestions([
+          'Palette',
+          'Palette with properties',
+          'Color sheet',
+        ])
+        result.setLoadingMessage('Palette layouts will be displayed here')
+        break
+      }
 
       default:
         return
@@ -61,7 +85,8 @@ figma.parameters.on(
   }
 )
 
-figma.on('run', ({ command, parameters }: RunEvent) => {
+
+figma.on('run', ({ parameters }: RunEvent) => {
   if (parameters == undefined) {
     figma.showUI(__html__, {
       width: 640,
@@ -69,15 +94,59 @@ figma.on('run', ({ command, parameters }: RunEvent) => {
       title: locals[lang].name,
       themeColors: true,
     })
-    figma.on('run', () => {
-      processSelection()
-      checkEditorType()
-      checkHighlightStatus(package_json.version)
-    })
+
+    figma.on('run', () => processSelection())
+    figma.on('run', () => checkEditorType())
+    figma.on('run', () => checkHighlightStatus(package_json.version))
+
     figma.on('selectionchange', () => processSelection())
 
     figma.on('run', async () => await checkPlanStatus())
     figma.on('selectionchange', async () => await checkPlanStatus())
+
+    figma.ui.onmessage = async (msg) => {
+      const i = 0,
+        j = 0
+    
+      const actions: ActionsList = {
+        CLOSE_HIGHLIGHT: () => closeHighlight(msg),
+        CREATE_PALETTE: () => createPalette(msg, palette),
+        UPDATE_SCALE: () => updateScale(msg, palette),
+        UPDATE_VIEW: () => updateView(msg, palette),
+        UPDATE_COLORS: () => updateColors(msg, palette),
+        UPDATE_THEMES: () => updateThemes(msg, palette),
+        SYNC_LOCAL_STYLES: () => {
+          notifications.splice(0, notifications.length)
+          createLocalStyles(palette, i)
+          updateLocalStyles(palette, i)
+          figma.notify(notifications.join('﹒'))
+        },
+        SYNC_LOCAL_VARIABLES: () => {
+          notifications.splice(0, notifications.length)
+          createLocalVariables(palette, i, j)
+          updateLocalVariables(palette, i, j)
+          figma.notify(notifications.join('﹒'))
+        },
+        EXPORT_PALETTE: () => {
+          msg.export === 'JSON' ? exportJson(palette) : null
+          msg.export === 'JSON_AMZN_STYLE_DICTIONARY'
+            ? exportJsonAmznStyleDictionary(palette)
+            : null
+          msg.export === 'JSON_TOKENS_STUDIO'
+            ? exportJsonTokensStudio(palette)
+            : null
+          msg.export === 'CSS' ? exportCss(palette) : null
+          msg.export === 'SWIFT' ? exportSwift(palette) : null
+          msg.export === 'XML' ? exportXml(palette) : null
+          msg.export === 'CSV' ? exportCsv(palette) : null
+        },
+        UPDATE_SETTINGS: () => updateSettings(msg, palette),
+        GET_PRO_PLAN: async () => await getProPlan(),
+        ENABLE_TRIAL: async () => await enableTrial(),
+      }
+    
+      return actions[msg.type]?.()
+    }
   } else {
     const selectedPreset = presets.find(preset => preset.name === parameters.preset)
     createPalette(
@@ -100,16 +169,16 @@ figma.on('run', ({ command, parameters }: RunEvent) => {
             }
           }),
           palette: {
-            name: parameters.name,
+            name: parameters.name == undefined ? '' : parameters.name,
             description: '',
             preset: presets.find(preset => preset.name === parameters.preset),
             scale: doLightnessScale(
-              selectedPreset!.scale,
-              selectedPreset!.min,
-              selectedPreset!.max
+              selectedPreset?.scale ?? [1, 2],
+              selectedPreset?.min ?? 0,
+              selectedPreset?.max ?? 100
             ),
-            colorSpace: 'LCH',
-            view: 'PALETTE',
+            colorSpace: parameters.space.toUpperCase().replace(' ', '_'),
+            view: parameters.view.toUpperCase().replace(' ', '_'),
             textColorsTheme: {
               lightColor: '#FFFFFF',
               darkColor: '#000000',
@@ -119,49 +188,6 @@ figma.on('run', ({ command, parameters }: RunEvent) => {
       },
       palette
     )
+    figma.closePlugin()
   }
 })
-
-figma.ui.onmessage = async (msg) => {
-  const i = 0,
-    j = 0
-
-  const actions: ActionsList = {
-    CLOSE_HIGHLIGHT: () => closeHighlight(msg),
-    CREATE_PALETTE: () => createPalette(msg, palette),
-    UPDATE_SCALE: () => updateScale(msg, palette),
-    UPDATE_VIEW: () => updateView(msg, palette),
-    UPDATE_COLORS: () => updateColors(msg, palette),
-    UPDATE_THEMES: () => updateThemes(msg, palette),
-    SYNC_LOCAL_STYLES: () => {
-      notifications.splice(0, notifications.length)
-      createLocalStyles(palette, i)
-      updateLocalStyles(palette, i)
-      figma.notify(notifications.join('﹒'))
-    },
-    SYNC_LOCAL_VARIABLES: () => {
-      notifications.splice(0, notifications.length)
-      createLocalVariables(palette, i, j)
-      updateLocalVariables(palette, i, j)
-      figma.notify(notifications.join('﹒'))
-    },
-    EXPORT_PALETTE: () => {
-      msg.export === 'JSON' ? exportJson(palette) : null
-      msg.export === 'JSON_AMZN_STYLE_DICTIONARY'
-        ? exportJsonAmznStyleDictionary(palette)
-        : null
-      msg.export === 'JSON_TOKENS_STUDIO'
-        ? exportJsonTokensStudio(palette)
-        : null
-      msg.export === 'CSS' ? exportCss(palette) : null
-      msg.export === 'SWIFT' ? exportSwift(palette) : null
-      msg.export === 'XML' ? exportXml(palette) : null
-      msg.export === 'CSV' ? exportCsv(palette) : null
-    },
-    UPDATE_SETTINGS: () => updateSettings(msg, palette),
-    GET_PRO_PLAN: async () => await getProPlan(),
-    ENABLE_TRIAL: async () => await enableTrial(),
-  }
-
-  return actions[msg.type]?.()
-}
