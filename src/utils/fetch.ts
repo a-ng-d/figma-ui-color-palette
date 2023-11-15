30213d60f19aba90f246e0a5555378bdb4550d41
Aurélien Grimaud <a.grimaud@involt.io>
2023-11-15 20:16:52 +0100

    refactor: use env var for config

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,7 +1,5 @@
-import { NOTION_API_KEY } from './../../env.s'
-
 export const notionOptions = {
-  Authorization: `Bearer ${NOTION_API_KEY}`,
+  Authorization: `Bearer ${process.env.REACT_APP_NOTION_API_KEY}`,
   Accept: 'application/json',
   'Notion-Version': '2022-06-28',
   'content-type': 'application/json',
