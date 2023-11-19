cfb4bd1e75b6fd41918f69a1c1c4dd403977c5be
Aurélien Grimaud <a.grimaud@involt.io>
2023-11-19 23:03:44 +0100

    fix: renable display of the secondary options

--- src/ui/components/ThemeItem.tsx
+++ src/ui/components/ThemeItem.tsx
@@ -46,8 +46,8 @@ export default class ThemeItem extends React.Component<Props, any> {
   }
 
   // Handlers
-  optionsHandler = (e: React.MouseEvent<Element, MouseEvent> & React.KeyboardEvent) => {
-    this.props.onCancellationSelection(e)
+  optionsHandler = () => {
+    this.props.onCancellationSelection
     this.setState({ hasMoreOptions: !this.state['hasMoreOptions'] })
   }
 
@@ -187,7 +187,7 @@ export default class ThemeItem extends React.Component<Props, any> {
                 icon="ellipsis"
                 state={this.state['hasMoreOptions'] ? 'selected' : ''}
                 feature="DISPLAY_MORE"
-                action={(e) => this.optionsHandler.bind(e)}
+                action={this.optionsHandler}
               />
             </Feature>
             <Button
