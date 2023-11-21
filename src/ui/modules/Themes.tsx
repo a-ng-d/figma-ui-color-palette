6e5437bd2f1cfec1b0a6a287c505d79918aa3595
Aurélien Grimaud <a.grimaud@involt.io>
2023-11-21 03:09:17 +0100

    refactor: avoid conflict between multiple events

--- src/ui/modules/Themes.tsx
+++ src/ui/modules/Themes.tsx
@@ -235,7 +235,7 @@ export default class Themes extends React.Component<Props, any> {
 
   selectionHandler = (e: any) => {
     const target = e.currentTarget
-    if (e.target.dataset.feature === 'DISPLAY_MORE') return
+    if (e.target.dataset.feature != undefined) return
     this.setState({
       selectedElement: {
         id: target.dataset.id,
