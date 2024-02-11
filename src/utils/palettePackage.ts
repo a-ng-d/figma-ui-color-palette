19f32a24213ff283a3003711c6e3004ee7381969
Aurélien Grimaud <a.grimaud@involt.io>
2024-02-12 00:42:55 +0100

    feat: add a comma to separate the name from the scale naming convention

--- src/utils/palettePackage.ts
+++ src/utils/palettePackage.ts
@@ -18,7 +18,7 @@ export const palette: PaletteConfiguration = {
 
 export const presets: Array<PresetConfiguration> = [
   {
-    name: 'Material Design 50-900 (Google)',
+    name: 'Material Design, 50-900 (Google)',
     scale: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
     min: 24,
     max: 96,
@@ -26,7 +26,7 @@ export const presets: Array<PresetConfiguration> = [
     id: 'MATERIAL',
   },
   {
-    name: 'Material 3 0-100 (Google)',
+    name: 'Material 3, 0-100 (Google)',
     scale: [100, 99, 95, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0],
     min: 0,
     max: 100,
@@ -34,7 +34,7 @@ export const presets: Array<PresetConfiguration> = [
     id: 'MATERIAL_3',
   },
   {
-    name: 'Tailwind 50-950',
+    name: 'Tailwind, 50-950',
     scale: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950],
     min: 16,
     max: 96,
@@ -42,7 +42,7 @@ export const presets: Array<PresetConfiguration> = [
     id: 'TAILWIND',
   },
   {
-    name: 'Ant Design 1-10',
+    name: 'Ant Design, 1-10',
     scale: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     min: 24,
     max: 96,
@@ -50,7 +50,7 @@ export const presets: Array<PresetConfiguration> = [
     id: 'ANT',
   },
   {
-    name: 'ADS Foundations 50-500 (Atlassian)',
+    name: 'ADS Foundations, 50-500 (Atlassian)',
     scale: [50, 75, 100, 200, 300, 400, 500],
     min: 24,
     max: 96,
@@ -69,7 +69,7 @@ export const presets: Array<PresetConfiguration> = [
     id: 'ADS_NEUTRAL',
   },
   {
-    name: 'Carbon 10-100 (IBM)',
+    name: 'Carbon, 10-100 (IBM)',
     scale: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
     min: 24,
     max: 96,
@@ -77,7 +77,7 @@ export const presets: Array<PresetConfiguration> = [
     id: 'CARBON',
   },
   {
-    name: 'Base 50-700 (Uber)',
+    name: 'Base, 50-700 (Uber)',
     scale: [50, 100, 200, 300, 400, 500, 600, 700],
     min: 24,
     max: 96,
