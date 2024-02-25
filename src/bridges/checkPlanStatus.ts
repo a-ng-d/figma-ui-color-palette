0e56d4be7fbdaceaee87e68094e30cb439e10eda
Aurélien Grimaud <a.grimaud@involt.io>
2024-02-25 22:35:37 +0100

    feat: extend expired trials

--- src/bridges/checkPlanStatus.ts
+++ src/bridges/checkPlanStatus.ts
@@ -1,13 +1,30 @@
 const checkPlanStatus = async () => {
   // figma.clientStorage.deleteAsync('trial_start_date')
-  const trialStartDate: number | undefined = await figma.clientStorage.getAsync(
+  //figma.clientStorage.deleteAsync('is_trial_extended')
+  /*figma.clientStorage
+        .setAsync('trial_start_date', 1703286000000)*/
+  
+  const
+    trialTime = 168
+
+  let trialStartDate: number | undefined = await figma.clientStorage.getAsync(
       'trial_start_date'
     ),
-    trialTime = 168
-  let consumedTime = 0,
+    isTrialExtended: boolean | undefined = await figma.clientStorage.getAsync(
+      'is_trial_extended'
+    ),
+    consumedTime = 0,
     trialStatus = 'UNUSED'
 
   if (trialStartDate != undefined) {
+    if (trialStartDate < 1708902000000 && (isTrialExtended == undefined || !isTrialExtended)) {
+      figma.clientStorage
+        .setAsync('trial_start_date', new Date().getTime() - 259200000)
+      figma.clientStorage
+        .setAsync('is_trial_extended', true)
+      trialStartDate = new Date().getTime() - 259200000
+      isTrialExtended = true
+    }
     consumedTime = (new Date().getTime() - trialStartDate) / 1000 / (60 * 60)
     trialStatus = consumedTime >= trialTime ? 'EXPIRED' : 'PENDING'
   }
