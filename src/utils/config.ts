ba711d286172d21ac1a9b42788d0c0c7632325a2
@a-ng-d <a.grimaud@involt.io>
2024-07-02 01:20:16 +0200

    fix: display the highlight for users who do not read unpatched version

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,6 +2,7 @@ import { Feature } from '../types/app'
 
 export const trialTime = 72
 export const oldTrialTime = 48
+export const releaseNotesVersion = '4.0.0'
 
 export const pageSize = 20
 export const workerUrl = process.env.REACT_APP_WORKER_URL as string
