e59cf7890f222e5bac8653cf273c7640e01bd165
@a-ng-d <a.grimaud@involt.io>
2025-06-27 17:01:33 +0200

    chore: migrate from webpack to vite for build process and update package.json
    - Updated package.json to include workspaces, private flag, and new scripts for package management.
    - Added new dependencies for Vite and removed Webpack-related dependencies.
    - Created vite.config.ts for Vite configuration, including Sentry integration and Preact setup.
    - Removed webpack.config.js as part of the migration.
    - Added vite-env.d.ts for Vite type definitions.
    - Updated tsconfig.json to include Vite configuration and paths for new structure.
    - Introduced a new subproject for ui-ui-color-palette.

---
+++ vite.config.ts
@@ -0,0 +1,63 @@
+import preact from '@preact/preset-vite'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const isDev = mode === 'development'
+  const isPlugin = process.env.IS_PLUGIN === 'true'
+
+  return {
+    plugins: [
+      preact(),
+      sentryVitePlugin({
+        org: 'yelbolt',
+        project: 'ui-color-palette',
+        authToken: env.SENTRY_AUTH_TOKEN,
+      }),
+      viteSingleFile(),
+    ],
+
+    resolve: {
+      alias: {
+        react: 'preact/compat',
+        'react-dom': 'preact/compat',
+        'react/jsx-runtime': 'preact/jsx-runtime',
+        '@ui-lib': path.resolve(
+          __dirname,
+          './packages/ui-ui-color-palette/src'
+        ),
+      },
+    },
+
+    build: {
+      target: 'es2015',
+      sourcemap: isDev,
+      minify: !isDev,
+      outDir: 'dist',
+      watch: isDev ? {} : null,
+      emptyOutDir: false,
+      ...(isPlugin
+        ? {
+            lib: {
+              entry: 'src/index.ts',
+              name: 'FigmaPlugin',
+              fileName: () => 'plugin.js',
+              formats: ['iife'],
+            },
+          }
+        : {
+            rollupOptions: {
+              input: 'index.html',
+              output: {
+                dir: 'dist',
+                entryFileNames: 'ui.js',
+                assetFileNames: 'assets/[name].[hash][extname]',
+              },
+            },
+          }),
+    },
+  }
+})
