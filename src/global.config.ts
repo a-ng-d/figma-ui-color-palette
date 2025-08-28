5ec7837199aad5a2ae3d0e6eb013ac3068099d19
@a-ng-d <a.grimaud@involt.io>
2025-08-29 00:17:31 +0200

    fix: update @a_ng_d/figmug-ui to version 1.17.2 and adjust subproject commit

--- src/global.config.ts
+++ src/global.config.ts
@@ -88,7 +88,6 @@ const specConfig: Record<
         'PRESETS_POLARIS',
         'PRESETS_CUSTOM_ADD',
         'SCALE_CHROMA',
-        'SCALE_HELPER_DISTRIBUTION',
         'THEMES',
         'THEMES_NAME',
         'THEMES_PARAMS',
