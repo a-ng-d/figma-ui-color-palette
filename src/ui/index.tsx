6b16bdb4dd58cea8098225ce0b67aa2c12795773
@a-ng-d <a.grimaud@involt.io>
2025-09-17 19:53:32 +0200

    fix: update @a_ng_d/figmug-ui version to 1.17.28 and refactor event listener for improved message handling

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -94,13 +94,23 @@ if (globalConfig.env.isSupabaseEnabled && supabaseAnonKey !== undefined)
   initSupabase(globalConfig.urls.databaseUrl, supabaseAnonKey)
 
 // Bridge Canvas <> UI
-window.addEventListener('message', (event) => {
-  const data = event.data.pluginMessage
-  const pluginEvent = new CustomEvent('pluginMessage', {
-    detail: data,
-  })
-  window.dispatchEvent(pluginEvent)
-})
+window.addEventListener(
+  'message',
+  (event: MessageEvent) => {
+    const pluginEvent = new CustomEvent('platformMessage', {
+      detail: event.data.pluginMessage,
+    })
+    window.dispatchEvent(pluginEvent)
+  },
+  false
+)
+
+window.addEventListener('pluginMessage', ((event: MessageEvent) => {
+  if (event instanceof CustomEvent && window.parent !== window) {
+    const { message, targetOrigin } = event.detail
+    parent.postMessage(message, targetOrigin)
+  }
+}) as EventListener)
 
 // Render
 root.render(
