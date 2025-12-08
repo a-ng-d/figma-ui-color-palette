2291e6659f3ac7e7b6982f53de21deceea3e16a4
@a-ng-d <a.grimaud@involt.io>
2025-12-08 12:53:20 +0100

    fix: set replaysSessionSampleRate to 0 for Sentry configuration

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -75,7 +75,7 @@ if (
     maxValueLength: 5000,
     maxBreadcrumbs: 150,
     tracesSampleRate: 1.0,
-    replaysSessionSampleRate: 0.01,
+    replaysSessionSampleRate: 0,
     replaysOnErrorSampleRate: 1.0,
     release: globalConfig.versions.pluginVersion,
   })
