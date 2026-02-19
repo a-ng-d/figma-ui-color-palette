274655856328ea5f8a50dfa035a7b2a141d31957
@a-ng-d <a.grimaud@involt.io>
2026-02-19 12:27:40 +0100

    refactor: remove unused createDocument import and simplify conditional logic in createPalette function

--- src/bridges/creations/createPalette.ts
+++ src/bridges/creations/createPalette.ts
@@ -7,7 +7,6 @@ import {
   ThemeConfiguration,
 } from '@a_ng_d/utils-ui-color-palette'
 import { tolgee } from '../..'
-import createDocument from './createDocument'
 
 interface Msg {
   data: {
@@ -97,14 +96,11 @@ const createPalette = async (msg: Msg, fromUI = true) => {
     `palette_${palette.meta.id}`,
     JSON.stringify(palette)
   )
-  if (fromUI) {
+  if (fromUI)
     figma.ui.postMessage({
       type: 'LOAD_PALETTE',
       data: palette,
     })
-
-    createDocument(palette.meta.id, 'PALETTE')
-  }
 
   await new Promise((r) => setTimeout(r, 1000))
   await figma.saveVersionHistoryAsync(
