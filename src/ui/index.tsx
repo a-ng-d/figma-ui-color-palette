b5380931dc11b9869b92d51dc06ab578cea78570
@a-ng-d <a.grimaud@involt.io>
2026-04-02 17:16:05 +0200

    fix: register cohort information in Mixpanel tracking

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -44,6 +44,10 @@ if (globalConfig.env.isMixpanelEnabled && mixpanelToken !== undefined) {
   })
   mixpanel.opt_in_tracking()
 
+  const now = new Date()
+  const cohort = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}`
+  mixpanel.register({ cohort })
+
   setMixpanelEnv(import.meta.env.MODE as 'development' | 'production')
   initMixpanel(mixpanel)
   setEditor(globalConfig.env.editor)
