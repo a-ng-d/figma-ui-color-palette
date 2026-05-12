41d23c443f1173dcbdc48f4dbc7346ff550b19bf
@a-ng-d <a.grimaud@involt.io>
2026-05-12 13:37:06 +0200

    feat: refactor loadUI function to streamline document editing process and improve UI responsiveness

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ figma.on('run', async ({ parameters }: RunEvent) => {
       'selectionchange',
       async () => await checkTrialStatus({ context: 'UI', plugin: __PLUGIN__ })
     )
-    loadUI().then(() => setTimeout(() => processSelection(), 3000))
+    loadUI()
   } else {
     const selectedPreset = presets.find(
       (preset) => preset.name === parameters.preset
